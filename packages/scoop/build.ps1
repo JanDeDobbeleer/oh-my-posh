0f3095321286cbe5b28268368d122d02e6c24489
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-11-01 17:22:00 +0100

    feat: add themes to scoop

--- packages/scoop/build.ps1
+++ packages/scoop/build.ps1
@@ -8,7 +8,10 @@ Param
     $LinuxSHA,
     [parameter(Mandatory = $true)]
     [string]
-    $WindowsSHA
+    $WindowsSHA,
+    [parameter(Mandatory = $true)]
+    [string]
+    $ThemesSHA
 )
 
 $content = Get-Content '.\oh-my-posh.json' -Raw
@@ -17,4 +20,5 @@ $content = $content.Replace('<HASH_LINUX>', $LinuxSHA)
 $content = $content.Replace('<HASH_WINDOWS>', $WindowsSHA)
 $fileHash = Get-FileHash post-install.ps1 -Algorithm SHA256
 $content = $content.Replace('<HASH_INSTALL_SCRIPT>', $fileHash.Hash)
+$content = $content.Replace('<HASH_THEMES>', $ThemesSHA)
 $content | Out-File -Encoding 'UTF8' '.\oh-my-posh.json'

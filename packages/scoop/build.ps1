16e9fd2c6eb3e158ff6902d70a5c9e487ed5cc1e
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-11-01 11:44:26 +0100

    fix: run post-install script

--- packages/scoop/build.ps1
+++ packages/scoop/build.ps1
@@ -1,20 +1,20 @@
 Param
 (
-    [parameter(Mandatory=$true)]
+    [parameter(Mandatory = $true)]
     [string]
     $Version,
-    [parameter(Mandatory=$true)]
+    [parameter(Mandatory = $true)]
     [string]
     $LinuxSHA,
-    [parameter(Mandatory=$true)]
+    [parameter(Mandatory = $true)]
     [string]
     $WindowsSHA
 )
 
-$content = Get-Content '.\oh-my-posh.json' -Raw
+$content = Get-Content '.\scoop.json' -Raw
 $content = $content.Replace('<VERSION>', $Version)
 $content = $content.Replace('<HASH_LINUX>', $LinuxSHA)
 $content = $content.Replace('<HASH_WINDOWS>', $WindowsSHA)
 $fileHash = Get-FileHash post-install.ps1 -Algorithm SHA256
 $content = $content.Replace('<HASH_INSTALL_SCRIPT>', $fileHash.Hash)
-$content | Out-File -Encoding 'UTF8' '.\oh-my-posh.json'
+$content | Out-File -Encoding 'UTF8' '.\scoop.json'

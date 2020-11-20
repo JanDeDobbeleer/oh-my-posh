cd15aa03c857d62e26973b4b036ca4ad62b21176
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-11-20 09:56:52 +0100

    feat: update scoop experience
    resolves #150

--- packages/scoop/build.ps1
+++ packages/scoop/build.ps1
@@ -2,23 +2,27 @@ Param
 (
     [parameter(Mandatory = $true)]
     [string]
-    $Version,
-    [parameter(Mandatory = $true)]
-    [string]
-    $LinuxSHA,
-    [parameter(Mandatory = $true)]
-    [string]
-    $WindowsSHA,
-    [parameter(Mandatory = $true)]
-    [string]
-    $ThemesSHA
+    $Version
 )
 
+New-Item -Path "." -Name "bin" -ItemType Directory
+Copy-Item -Path "../../themes" -Destination "./bin" -Recurse
+
+# Download the files and pack them
+@{name = 'posh-windows-amd64.exe'; outName = 'oh-my-posh.exe' }, @{name = 'posh-linux-amd64'; outName = 'oh-my-posh-wsl' } | ForEach-Object -Process {
+    $download = "https://github.com/jandedobbeleer/oh-my-posh3/releases/download/v$Version/$($_.name)"
+    Invoke-WebRequest $download -Out "./bin/$($_.outName)"
+}
+$compress = @{
+    Path             = "./bin/*"
+    CompressionLevel = "Fastest"
+    DestinationPath  = "./posh-windows-wsl-amd64.7z"
+}
+Compress-Archive @compress
+$zipHash = Get-FileHash ./posh-windows-wsl-amd64.7z -Algorithm SHA256
 $content = Get-Content '.\oh-my-posh.json' -Raw
 $content = $content.Replace('<VERSION>', $Version)
-$content = $content.Replace('<HASH_LINUX>', $LinuxSHA)
-$content = $content.Replace('<HASH_WINDOWS>', $WindowsSHA)
-$fileHash = Get-FileHash post-install.ps1 -Algorithm SHA256
-$content = $content.Replace('<HASH_INSTALL_SCRIPT>', $fileHash.Hash)
-$content = $content.Replace('<HASH_THEMES>', $ThemesSHA)
-$content | Out-File -Encoding 'UTF8' '.\oh-my-posh.json'
+$content = $content.Replace('<HASH>', $zipHash.Hash)
+$content | Out-File -Encoding 'UTF8' './oh-my-posh.json'
+
+Remove-Item ./bin/ -Recurse

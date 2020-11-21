f389d6342cf783353bc33e9d09391c888fa4b879
Spencer Hedrick <shedric1@asu.edu>
2020-11-21 02:27:29 -0800

    feat: add inline background color syntax

--- renderer_test.go
+++ renderer_test.go
@@ -41,6 +41,27 @@ func TestWriteColorOverride(t *testing.T) {
 	assert.NotContains(t, renderer.string(), "<#ff5733>")
 }
 
+func TestWriteColorOverrideBackground(t *testing.T) {
+	renderer := &Renderer{
+		Buffer: new(bytes.Buffer),
+	}
+	text := "This is white, <,#000000>this is black</>, white again"
+	renderer.init("pwsh")
+	renderer.write("#193549", "#ff5733", text)
+	assert.NotContains(t, renderer.string(), "000000")
+}
+
+func TestWriteColorOverrideBoth(t *testing.T) {
+	renderer := &Renderer{
+		Buffer: new(bytes.Buffer),
+	}
+	text := "This is white, <#000000,#ffffff>this is black</>, white again"
+	renderer.init("pwsh")
+	renderer.write("#193549", "#ff5733", text)
+	assert.NotContains(t, renderer.string(), "ffffff")
+	assert.NotContains(t, renderer.string(), "000000")
+}
+
 func TestWriteColorTransparent(t *testing.T) {
 	renderer := &Renderer{
 		Buffer: new(bytes.Buffer),

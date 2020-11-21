56e799a6b0945cd4e6b96f953a1bdccc020fcccc
Spencer Hedrick <shedric1@asu.edu>
2020-11-21 05:46:30 -0800

    chore: new unit tests for 16 color background overrides

--- renderer_test.go
+++ renderer_test.go
@@ -51,6 +51,18 @@ func TestWriteColorOverrideBackground(t *testing.T) {
 	assert.NotContains(t, renderer.string(), "000000")
 }
 
+func TestWriteColorOverrideBackground16(t *testing.T) {
+	renderer := &Renderer{
+		Buffer: new(bytes.Buffer),
+	}
+	text := "This is default <,white> this background is changed</> default again"
+	renderer.init("pwsh")
+	renderer.write("#193549", "#ff5733", text)
+	assert.NotContains(t, renderer.string(), "white")
+	assert.NotContains(t, renderer.string(), "</>")
+	assert.NotContains(t, renderer.string(), "<,")
+}
+
 func TestWriteColorOverrideBoth(t *testing.T) {
 	renderer := &Renderer{
 		Buffer: new(bytes.Buffer),
@@ -60,6 +72,17 @@ func TestWriteColorOverrideBoth(t *testing.T) {
 	renderer.write("#193549", "#ff5733", text)
 	assert.NotContains(t, renderer.string(), "ffffff")
 	assert.NotContains(t, renderer.string(), "000000")
+}
+
+func TestWriteColorOverrideBoth16(t *testing.T) {
+	renderer := &Renderer{
+		Buffer: new(bytes.Buffer),
+	}
+	text := "This is white, <black,white>this is black</>, white again"
+	renderer.init("pwsh")
+	renderer.write("#193549", "#ff5733", text)
+	assert.NotContains(t, renderer.string(), "<black,white>")
+	assert.NotContains(t, renderer.string(), "</>")
 }
 
 func TestWriteColorTransparent(t *testing.T) {

732035dfa1bffe62e3c14e244a1cef1f853efda6
PIYUSH194 <piyush194@hotmail.com>
2020-11-30 18:35:59 +0530

    feat: support Powershell 5

--- packages/powershell/oh-my-posh/oh-my-posh.psm1
+++ packages/powershell/oh-my-posh/oh-my-posh.psm1
@@ -20,7 +20,7 @@ function Get-PoshCommand {
 }
 
 # Set the right binary to executable before doing anything else
-if (!$IsWindows) {
+if ($PSVersionTable.PSEdition -eq "Core" -and !$IsWindows) {
     $executable = Get-PoshCommand
     Invoke-Expression -Command "chmod +x $executable"
 }

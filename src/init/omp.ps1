23eb73cd699d1247f2e29faa34579b1ade61e512
TravisTX <travistx@gmail.com>
2020-12-30 07:49:08 -0700

    fix: fix lastexitcode

--- src/init/omp.ps1
+++ src/init/omp.ps1
@@ -56,10 +56,10 @@ function Set-GitStatus {
     $standardOut = $process.StandardOutput.ReadToEnd()
     $process.WaitForExit()
     $standardOut
+    Set-GitStatus
     $global:LASTEXITCODE = $realLASTEXITCODE
     #remove temp variables
     Remove-Variable realLASTEXITCODE -Confirm:$false
     Remove-Variable lastCommandSuccess -Confirm:$false
-    Set-GitStatus
 }
 Set-Item -Path Function:prompt -Value $Prompt -Force

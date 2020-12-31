5fa33f4c4f45a410b88997ed16b5b41f3edde847
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-12-31 09:00:32 +0100

    fix: use commandError on cmd failure
    relates to #285

--- src/environment.go
+++ src/environment.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -58,11 +56,12 @@ type environment struct {
 }
 
 type commandError struct {
+	err      string
 	exitCode int
 }
 
 func (e *commandError) Error() string {
-	return fmt.Sprintf("%d", e.exitCode)
+	return e.err
 }
 
 func (env *environment) getenv(key string) string {
@@ -176,15 +175,24 @@ func (env *environment) runCommand(command string, args ...string) (string, erro
 	cmd := exec.Command(command, args...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatal(err)
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 666,
+		}
 	}
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		log.Fatal(err)
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 667,
+		}
 	}
 	err = cmd.Start()
 	if err != nil {
-		return "", err
+		return "", &commandError{
+			err:      err.Error(),
+			exitCode: 668,
+		}
 	}
 	defer func() {
 		_ = cmd.Process.Kill()
@@ -192,7 +200,10 @@ func (env *environment) runCommand(command string, args ...string) (string, erro
 	stdoutString := getOutputString(stdout)
 	stderrString := getOutputString(stderr)
 	if stderrString != "" {
-		return "", errors.New(stderrString)
+		return "", &commandError{
+			err:      stderrString,
+			exitCode: cmd.ProcessState.ExitCode(),
+		}
 	}
 	return stdoutString, nil
 }

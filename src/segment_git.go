d5692649c355c669bf87afa6410aa8816a6f0a55
evilz <evilznet@gmail.com>
2021-03-10 18:42:24 +0100

    fix: slow git status on wsl2

--- src/segment_git.go
+++ src/segment_git.go
@@ -272,7 +272,7 @@ func (g *git) getStatusColor(defaultValue string) string {
 
 func (g *git) getGitCommandOutput(args ...string) string {
 	gitCommand := "git"
-	if g.env.getRuntimeGOOS() == windowsPlatform {
+	if g.env.getRuntimeGOOS() == windowsPlatform || (g.env.isWsl() && strings.HasPrefix(g.env.getcwd(), "/mnt/")) {
 		gitCommand = "git.exe"
 	}
 	args = append([]string{"--no-optional-locks", "-c", "core.quotepath=false", "-c", "color.status=false"}, args...)

95068dd224f70605fd823bcc95ca3955c1bad8ad
Chris Donnelly <chris.donnelly@coxautoinc.com>
2021-06-16 11:52:42 -0500

    feat(dotnet): correct exit code on Windows
    dotnet on windows returns a 32-bit exit code.
    
    https://github.com/dotnet/runtime/blob/main/docs/design/features/host-error-codes.md

--- src/segment_dotnet.go
+++ src/segment_dotnet.go
@@ -8,15 +8,19 @@ const (
 	// UnsupportedDotnetVersionIcon is displayed when the dotnet version in
 	// the current folder isn't supported by the installed dotnet SDK set.
 	UnsupportedDotnetVersionIcon Property = "unsupported_version_icon"
+
+	dotnetExitCodeUnix    = 145
+	dotnetExitCodeWindows = -2147450735
 )
 
 func (d *dotnet) string() string {
 	version := d.language.string()
 
-	// Exit code 145 is a special indicator that dotnet
+	// Exit codes 145 and 0x80008091 are special indicators that dotnet
 	// ran, but the current project config settings specify
 	// use of an SDK that isn't installed.
-	if d.language.exitCode == 145 {
+	exitCode := d.language.exitCode
+	if exitCode == dotnetExitCodeWindows || exitCode == dotnetExitCodeUnix {
 		return d.language.props.getString(UnsupportedDotnetVersionIcon, "\uf071 ")
 	}
 

f412ef66444deddfba489016a6dcb1d7ab50fe75
Aaron Sherber <aaron@sherber.com>
2021-09-17 15:48:00 -0400

    feat: allow for custom text in place of shell names

--- src/segment_shell_test.go
+++ src/segment_shell_test.go
@@ -17,3 +17,28 @@ func TestWriteCurrentShell(t *testing.T) {
 	}
 	assert.Equal(t, expected, s.string())
 }
+
+func TestUseMappedShellNames(t *testing.T) {
+	cases := []struct {
+		Shell    string
+		Expected string
+	}{
+		{Shell: "zsh", Expected: "zsh"},
+		{Shell: "pwsh", Expected: "PS"},
+		{Shell: "PWSH", Expected: "PS"},
+	}
+	for _, tc := range cases {
+		env := new(MockedEnvironment)
+		env.On("getShellName", nil).Return(tc.Expected, nil)
+		s := &shell{
+			env: env,
+			props: &properties{
+				values: map[Property]interface{}{
+					MappedShellNames: map[string]string{"pwsh": "PS"},
+				},
+			},
+		}
+		got := s.string()
+		assert.Equal(t, tc.Expected, got)
+	}
+}

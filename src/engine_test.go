6bef088b83c5567e92cc157e22311d2ef7a685fe
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-09-17 08:15:06 +0200

    feat: allow auto hiding of rprompt on Windows

--- src/engine_test.go
+++ src/engine_test.go
@@ -22,7 +22,7 @@ func TestCanWriteRPrompt(t *testing.T) {
 		{Case: "Terminal > Prompt enabled edge", Expected: true, TerminalWidth: 200, PromptLength: 100, RPromptLength: 70},
 		{Case: "Terminal > Prompt disabled no breathing", Expected: false, TerminalWidth: 200, PromptLength: 100, RPromptLength: 71},
 		{Case: "Prompt > Terminal enabled", Expected: true, TerminalWidth: 200, PromptLength: 300, RPromptLength: 70},
-		{Case: "Prompt > Terminal disabled no breathing", Expected: true, TerminalWidth: 200, PromptLength: 300, RPromptLength: 80},
+		{Case: "Prompt > Terminal disabled no breathing", Expected: false, TerminalWidth: 200, PromptLength: 300, RPromptLength: 80},
 		{Case: "Prompt > Terminal disabled no room", Expected: true, TerminalWidth: 200, PromptLength: 400, RPromptLength: 80},
 	}
 
@@ -38,6 +38,6 @@ func TestCanWriteRPrompt(t *testing.T) {
 		engine.rprompt = strings.Repeat("x", tc.RPromptLength)
 		engine.console.WriteString(strings.Repeat("x", tc.PromptLength))
 		got := engine.canWriteRPrompt()
-		assert.Equal(t, tc.Expected, got)
+		assert.Equal(t, tc.Expected, got, tc.Case)
 	}
 }

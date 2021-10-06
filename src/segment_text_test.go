d0816773a51c0ab62016d91294902fe7bd38ed91
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-10-06 21:50:52 +0200

    fix(text): disable when text resolves to empty

--- src/segment_text_test.go
+++ src/segment_text_test.go
@@ -8,15 +8,18 @@ import (
 
 func TestTextSegment(t *testing.T) {
 	cases := []struct {
-		Case           string
-		ExpectedString string
-		Text           string
+		Case             string
+		ExpectedString   string
+		Text             string
+		ExpectedDisabled bool
 	}{
 		{Case: "standard text", ExpectedString: "hello", Text: "hello"},
 		{Case: "template text with env var", ExpectedString: "hello world", Text: "{{ .Env.HELLO }} world"},
 		{Case: "template text with shell name", ExpectedString: "hello world from terminal", Text: "{{ .Env.HELLO }} world from {{ .Shell }}"},
 		{Case: "template text with folder", ExpectedString: "hello world in posh", Text: "{{ .Env.HELLO }} world in {{ .Folder }}"},
 		{Case: "template text with user", ExpectedString: "hello Posh", Text: "{{ .Env.HELLO }} {{ .User }}"},
+		{Case: "empty text", Text: "", ExpectedDisabled: true},
+		{Case: "empty template result", Text: "{{ .Env.WORLD }}", ExpectedDisabled: true},
 	}
 
 	for _, tc := range cases {
@@ -27,6 +30,7 @@ func TestTextSegment(t *testing.T) {
 		env.On("isRunningAsRoot", nil).Return(true)
 		env.On("getShellName", nil).Return("terminal")
 		env.On("getenv", "HELLO").Return("hello")
+		env.On("getenv", "WORLD").Return("")
 		env.On("getCurrentUser", nil).Return("Posh")
 		env.On("getHostName", nil).Return("MyHost", nil)
 		props := &properties{
@@ -38,6 +42,7 @@ func TestTextSegment(t *testing.T) {
 			env:   env,
 			props: props,
 		}
+		assert.Equal(t, tc.ExpectedDisabled, !txt.enabled(), tc.Case)
 		assert.Equal(t, tc.ExpectedString, txt.string(), tc.Case)
 	}
 }

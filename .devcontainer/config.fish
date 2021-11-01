d4c743b594deffb863b9edf70ac847841822c08f
Alan Fregtman <941331+darkvertex@users.noreply.github.com>
2021-11-01 20:53:25 +0000

    feat(devcontainer): deploy to Powershell, Bash, Fish and ZSH

---
+++ .devcontainer/config.fish
@@ -0,0 +1,4 @@
+# Activate oh-my-posh prompt:
+oh-my-posh --init --shell fish --config $POSHTHEMES_ROOT/$DEFAULT_POSH_THEME.omp.json | source
+
+# NOTE: You can override the above env vars from the devcontainer.json "args" under the "build" key.

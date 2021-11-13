2ee89a18f8f268af865ce08c8dd0dc66dc6e216d
lnu <l.nullens@gmail.com>
2021-11-13 23:46:06 +0100

    feat(sysinfo): sysinfo segment
    cpu, mem, load, ...

---
+++ docs/docs/sysinfo.md
@@ -0,0 +1,49 @@
+---
+id: sysinfo
+title: SysInfo
+sidebar_label: SysInfo
+---
+
+## SysInfo
+
+Display SysInfo.
+
+## Sample Configuration
+
+```json
+{
+  "type":"sysinfo",
+  "powerline_symbol": "",
+  "foreground": "#ffffff",
+  "background": "#8f43f3",
+  "invert_powerline": true,
+  "properties": {
+    "prefix": " \uf85a ",
+    "postfix": "% ",
+    "precision": 2,
+    "template":"{{ round .PhysicalPercentUsed .Precision }}"
+  },
+  "style":"powerline"
+},
+```
+
+## Properties
+
+- Precision: `int` - The precision used for any float values - defaults to 2
+- template: `string` - A go [text/template][go-text-template] template extended with [sprig][sprig] utilizing the
+properties below - defaults to `{{ round .PhysicalPercentUsed .Precision }}> `
+
+## Template Properties
+
+- `.PhysicalTotalMemory`: `int` - is the total of used physical memory
+- `.PhysicalFreeMemory`: `int` - is the total of free physical memory
+- `.PhysicalPercentUsed`: `float64` - is the percentage of physical memory in usage
+- `.SwapTotalMemory`: `int` - is the total of used swap memory
+- `.SwapFreeMemory`: `int` - is the percentage of swap memory in usage
+- `.SwapPercentUsed`: `float64` - is the current user root/admin or not
+- `.Load1`: `float64` - is the current load1 (can be empty on windows)
+- `.Load5`: `float64` - is the current load5 (can be empty on windows)
+- `.Load15`: `float64` - is the current load15 (can be empty on windows)
+- `.CPU`: `[]struct` - an array of [InfoStat][cpuinfo] object, you can use any property it has e.g. `(index .CPU 0).Cores`
+
+[cpuinfo]: https://github.com/shirou/gopsutil/blob/78065a7ce2021f6a78c8d6f586a2683ba501dcec/cpu/cpu.go#L32
---
+++ src/segment_sysinfo.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	cpu "github.com/shirou/gopsutil/v3/cpu"
+	load "github.com/shirou/gopsutil/v3/load"
+	mem "github.com/shirou/gopsutil/v3/mem"
+)
+
+type sysinfo struct {
+	props     *properties
+	env       environmentInfo
+	Precision int
+	// mem
+	PhysicalTotalMemory uint64
+	PhysicalFreeMemory  uint64
+	PhysicalPercentUsed float64
+	SwapTotalMemory     uint64
+	SwapFreeMemory      uint64
+	SwapPercentUsed     float64
+	// cpu
+	Times float64
+	CPU   []cpu.InfoStat
+	// load
+	Load1  float64
+	Load5  float64
+	Load15 float64
+}
+
+const (
+	// Precision number of decimal places to show
+	Precision Property = "precision"
+)
+
+func (s *sysinfo) enabled() bool {
+	if s.PhysicalPercentUsed == 0 && s.SwapPercentUsed == 0 {
+		return false
+	}
+	return true
+}
+
+func (s *sysinfo) string() string {
+	// keep old memory segment template
+	segmentTemplate := s.props.getString(SegmentTemplate, "{{ round .PhysicalPercentUsed .Precision }}")
+	template := &textTemplate{
+		Template: segmentTemplate,
+		Context:  s,
+		Env:      s.env,
+	}
+	text, err := template.render()
+	if err != nil {
+		return err.Error()
+	}
+	return text
+}
+
+func (s *sysinfo) init(props *properties, env environmentInfo) {
+	s.props = props
+	s.env = env
+	s.Precision = s.props.getInt(Precision, 2)
+	// mem
+	memStat, err := mem.VirtualMemory()
+	if err == nil {
+		s.PhysicalTotalMemory = memStat.Total
+		s.PhysicalFreeMemory = memStat.Free
+		s.PhysicalPercentUsed = memStat.UsedPercent
+	}
+	swapStat, err := mem.SwapMemory()
+	if err == nil {
+		s.SwapTotalMemory = swapStat.Total
+		s.SwapFreeMemory = swapStat.Free
+		s.SwapPercentUsed = swapStat.UsedPercent
+	}
+	// load
+	loadStat, err := load.Avg()
+	if err == nil {
+		s.Load1 = loadStat.Load1
+		s.Load5 = loadStat.Load5
+		s.Load15 = loadStat.Load15
+	}
+	// times
+	processorTimes, err := cpu.Percent(0, false)
+	if err == nil {
+		s.Times = processorTimes[0]
+	}
+	// cpu
+	processors, err := cpu.Info()
+	if err == nil {
+		s.CPU = processors
+	}
+}

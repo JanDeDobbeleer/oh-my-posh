a548e9f13f97df97b49a1da9fd15a31d0827d6e5
will <billyrayleach@outlook.com>
2021-11-16 22:21:55 +0000

    first pass to get reg key open/query handling going. - open key - query key for size and type

--- src/segment_regquery.go
+++ src/segment_regquery.go
@@ -3,17 +3,16 @@ package main
 type regquery struct {
     props          *properties
     env            environmentInfo
-	content			string;
+	content			string
 }
 
 const (
-     //NewProp switches something
      RegistryPath Property = "registry_path"
 	 RegistryKey Property = "registry_key"
 )
 
 func (r *regquery) string() string {
-    newText := "Yes"//r.content;
+    newText := r.content;
     return newText
 }
 
--- src/segment_regquery_windows.go
+++ src/segment_regquery_windows.go
@@ -3,11 +3,42 @@ package main
 
 import (
 	"fmt"
+    "golang.org/x/sys/windows"
 )
 
 func (r *regquery) enabled() bool {
-	// Call registry code and full out "content" string.	
+    var ret bool = false;
+    r.content = "fail";
 
-	r.content = fmt.Sprintf("Reg query %s /v %s", RegistryPath ,RegistryKey);
-	return true;
+	// Call registry code and full out "content" string.	
+    registryPath := r.props.getString(RegistryPath, "")
+    registryKey := r.props.getString(RegistryKey, "")
+
+    regPathUTF16, regPathErr :=  windows.UTF16FromString(registryPath);
+
+    if (regPathErr == nil) {
+        var hKey windows.Handle;
+
+        regOpenErr := windows.RegOpenKeyEx(windows.HKEY_LOCAL_MACHINE, &regPathUTF16[0], 0, windows.KEY_READ, &hKey)
+
+        if (regOpenErr == nil) {
+            regKeyUTF16, regKeyErr := windows.UTF16FromString(registryKey);
+
+            if (regKeyErr == nil){
+                // size first...
+                var keyBufType uint32;
+                var keyBufSize uint32;
+
+                regQueryErr := windows.RegQueryValueEx(hKey, &regKeyUTF16[0], nil, &keyBufType, nil, &keyBufSize);
+
+                if (regQueryErr == nil) {        
+                    r.content = fmt.Sprintf("Reg query do it! %s /v %s, type: %d", registryPath ,registryKey, keyBufType);
+                    ret = true;
+                }
+            }
+        }
+
+        windows.RegCloseKey(hKey);
+    }
+    return ret;
 }

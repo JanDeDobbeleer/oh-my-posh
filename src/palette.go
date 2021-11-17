d67e6e86d1f71b23578ac2da61fc3e2595f9cfcd
Yehor Borkov <yehor.borkov@gmail.com>
2021-11-17 18:19:36 +0200

    feat(palette): ignoring non-palette colors

--- src/palette.go
+++ src/palette.go
@@ -11,6 +11,10 @@ const (
 )
 
 func (p Palette) resolveColor(colorName string) (string, error) {
+	if !strings.HasPrefix(colorName, paletteColorPrefix) {
+		return colorName, nil
+	}
+
 	paletteName := strings.ReplaceAll(colorName, paletteColorPrefix, "")
 
 	if paletteColor, ok := p[paletteName]; ok {
--- src/palette_test.go
+++ src/palette_test.go
@@ -16,21 +16,41 @@ var (
 	}
 )
 
+type TestPaletteRequest struct {
+	Case     string
+	Request  string
+	Expected string
+}
+
 func TestPaletteShouldResolveColorFromTestPalette(t *testing.T) {
-	cases := []struct {
-		Case     string
-		Request  string
-		Expected string
-	}{
+	cases := []TestPaletteRequest{
 		{Case: "Red", Request: "palette:red", Expected: "#FF0000"},
 		{Case: "Green", Request: "palette:green", Expected: "#00FF00"},
 		{Case: "Red", Request: "palette:blue", Expected: "#0000FF"},
 	}
 
 	for _, tc := range cases {
-		actual, err := testPalette.resolveColor(tc.Request)
+		testPaletteRequest(t, tc)
+	}
+}
 
-		assert.Nil(t, err, "expected no error")
-		assert.Equal(t, tc.Expected, actual, "expected different color value")
+func testPaletteRequest(t *testing.T, tc TestPaletteRequest) {
+	actual, err := testPalette.resolveColor(tc.Request)
+
+	assert.Nil(t, err, "expected no error")
+	assert.Equal(t, tc.Expected, actual, "expected different color value")
+}
+
+func TestPaletteShouldIgnoreNonPaletteColors(t *testing.T) {
+	cases := []TestPaletteRequest{
+		{Case: "Deep puprple", Request: "#1F1137", Expected: "#1F1137"},
+		{Case: "Palette red", Request: "palette:red", Expected: "#FF0000"},
+		{Case: "Light red", Request: "#D55252", Expected: "#D55252"},
+		{Case: "ANSI black", Request: "black", Expected: "black"},
+		{Case: "Palette black", Request: "palette:black", Expected: "#000000"},
+	}
+
+	for _, tc := range cases {
+		testPaletteRequest(t, tc)
 	}
 }

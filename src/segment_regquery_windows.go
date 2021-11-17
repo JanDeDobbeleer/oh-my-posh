12d41f5b5f6fa5e5dd0d1256c745b903393c5135
will <billyrayleach@outlook.com>
2021-11-17 10:12:50 +0000

    test flow reading values out of the registry for real.

--- src/segment_regquery_windows.go
+++ src/segment_regquery_windows.go
@@ -4,13 +4,14 @@ package main
 import (
 	"fmt"
     "golang.org/x/sys/windows"
+    "unsafe"
 )
 
 func (r *regquery) enabled() bool {
-    var ret bool = false;
-    r.content = "fail";
+    var ret bool = false
+    r.content = "fail"
 
-	// Call registry code and full out "content" string.	
+	// Call registry code and fill out "content" string.	
     registryPath := r.props.getString(RegistryPath, "")
     registryKey := r.props.getString(RegistryKey, "")
 
@@ -26,14 +27,35 @@ func (r *regquery) enabled() bool {
 
             if (regKeyErr == nil){
                 // size first...
-                var keyBufType uint32;
-                var keyBufSize uint32;
+                var keyBufType uint32
+                var keyBufSize uint32
 
                 regQueryErr := windows.RegQueryValueEx(hKey, &regKeyUTF16[0], nil, &keyBufType, nil, &keyBufSize);
 
-                if (regQueryErr == nil) {        
-                    r.content = fmt.Sprintf("Reg query do it! %s /v %s, type: %d", registryPath ,registryKey, keyBufType);
-                    ret = true;
+                if (regQueryErr == nil) {
+                    
+                    var keyBuf = make([]byte, keyBufSize, keyBufSize);
+
+                    regQueryErr := windows.RegQueryValueEx(hKey, &regKeyUTF16[0], nil, &keyBufType, &keyBuf[0], &keyBufSize);
+
+                    if (regQueryErr == nil) {
+
+                        switch (keyBufType) {
+                            case windows.REG_SZ:
+                                var uint16p *uint16
+                                uint16p = (*uint16)(unsafe.Pointer(&keyBuf[0]))  // nasty casty
+                                s := windows.UTF16PtrToString(uint16p)
+                                r.content = s
+                                ret = true
+                            case windows.REG_DWORD:
+                                var uint32p *uint32
+                                uint32p = (*uint32)(unsafe.Pointer(&keyBuf[0])) // more casting goodness
+                                r.content = fmt.Sprintf("0x%08X", *uint32p)
+                                ret = true
+                            default:
+                                r.content = fmt.Sprintf("default: %d, %d %d", keyBufType, keyBufSize, keyBuf[0])
+                            }
+                    }
                 }
             }
         }

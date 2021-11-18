56e958fd20ce1367649de7e42e5c321ce5c3fab5
Mike Sigsworth <mikesigsworth@gmail.com>
2021-11-18 07:50:22 +0000

    devcontainer runs Debian with no wifi interface, must use windows/wsl

--- src/segment_wifi.go
+++ src/segment_wifi.go
@@ -1,7 +1,5 @@
 package main
 
-import "net"
-
 type wifi struct {
 	props *properties
 	env   environmentInfo
@@ -19,12 +17,23 @@ func (w *wifi) enabled() bool {
 func (w *wifi) string() string {
 	// newText := w.props.getString(NewProp, "\uEFF1")
 	// return newText
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		return "WiFi failed"
+
+	// If in Linux, who knows. Gonna need a physical linux machine to develop this since the wlan interface isn't available in wsl
+	// But possible solution is to cat /proc/net/wireless, and if that file doesn't exist then don't show the segment
+
+	// In Windows and in wsl you can use the command `netsh.exe wlan show interfaces`. Note the .exe is important for this to work in wsl.
+
+	// if w.env.getPlatform() == windowsPlatform || w.env.isWsl() {
+	// 	output := w.env.runShellCommand(w.env.getShellName(), "netsh.exe wlan show interfaces")
+	// 	return s
+	// }
+	// return "WiFi"
+
+	s := w.env.getPlatform()
+	if w.env.isWsl() {
+		s += " isWsl"
 	}
-	// https://github.com/skycoin/examples/blob/af669cb42b6cc73e7fef5198fc9b5c4b6afce357/aether/wifi/wifi.go
-	return "WiFi"
+	return s
 }
 
 func (w *wifi) init(props *properties, env environmentInfo) {
--- src/segment_wifi_test.go
+++ src/segment_wifi_test.go
@@ -8,6 +8,9 @@ import (
 
 func bootStrapWifiTest() *wifi {
 	env := new(MockedEnvironment)
+	//w.env.getPlatform() == windowsPlatform || w.env.isWsl()
+	env.On("getPlatform").Return(windowsPlatform)
+	env.On("isWsl").Return(false)
 	// env.On("hasCommand", "terraform").Return(args.hasTfCommand)
 	// env.On("hasFolder", ".terraform").Return(args.hasTfFolder)
 	// env.On("runCommand", "terraform", []string{"workspace", "show"}).Return(args.workspaceName, nil)
@@ -23,7 +26,7 @@ func TestSomething(t *testing.T) {
 	assert.True(t, wifi.enabled())
 }
 
-func TestInterfaces(t *testing.T) {
+func TestString(t *testing.T) {
 	wifi := bootStrapWifiTest()
 	assert.NotNil(t, wifi.string())
 }

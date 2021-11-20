43a442fd67a20f439d81ab438137690cd7f3ed05
Mike Sigsworth <mikesigsworth@gmail.com>
2021-11-20 00:05:28 -0700

    It works!

--- src/segment_wifi.go
+++ src/segment_wifi.go
@@ -1,39 +1,100 @@
 package main
 
+import (
+	"regexp"
+	"strings"
+)
+
 type wifi struct {
-	props *properties
-	env   environmentInfo
+	props              *properties
+	env                environmentInfo
+	State              string
+	SSID               string
+	RadioType          string
+	AuthenticationType string
+	Channel            string
+	ReceiveRate        string
+	TransmitRate       string
+	SignalStrength     string
 }
 
 const (
-	//NewProp switches something
-	NewProp Property = "WiFi"
+	State              = "State"
+	SSID               = "SSID"
+	RadioType          = "Radio type"
+	AuthenticationType = "Authentication"
+	Channel            = "Channel"
+	ReceiveRate        = "Receive rate (Mbps)"
+	TransmitRate       = "Transmit rate (Mbps)"
+	SignalStrength     = "Signal"
 )
 
 func (w *wifi) enabled() bool {
+	// If in Linux, who wnows. Gonna need a physical linux machine to develop this since the wlan interface isn't available in wsl
+	// But possible solution is to cat /proc/net/wireless, and if that file doesn't exist then don't show the segment
+	// In Windows and in wsl you can use the command `netsh.exe wlan show interfaces`. Note the .exe is important for this to worw in wsl.
+	if w.env.getPlatform() == windowsPlatform || w.env.isWsl() {
+		cmd := "netsh"
+		if !w.env.hasCommand(cmd) {
+			return false
+		}
+		cmdResult, err := w.env.runCommand(cmd, "wlan", "show", "interfaces")
+		displayError := w.props.getBool(DisplayError, false)
+		if err != nil && displayError {
+			w.State = "WIFI ERR"
+			// w.Namespace = w.Context
+			return true
+		}
+		if err != nil {
+			return false
+		}
+
+		regex := regexp.MustCompile(`(.+) : (.+)`)
+		lines := strings.Split(cmdResult, "\n")
+		for _, line := range lines[3 : len(lines)-3] {
+			matches := regex.FindStringSubmatch(line)
+			if len(matches) != 3 {
+				continue
+			}
+			name := strings.TrimSpace(matches[1])
+			value := strings.TrimSpace(matches[2])
+
+			switch name {
+			case State:
+				w.State = value
+			case SSID:
+				w.SSID = value
+			case RadioType:
+				w.RadioType = value
+			case AuthenticationType:
+				w.AuthenticationType = value
+			case Channel:
+				w.Channel = value
+			case ReceiveRate:
+				w.ReceiveRate = strings.Split(value, ".")[0]
+			case TransmitRate:
+				w.TransmitRate = strings.Split(value, ".")[0]
+			case SignalStrength:
+				w.SignalStrength = value
+			}
+		}
+	}
+
 	return true
 }
 
 func (w *wifi) string() string {
-	// newText := w.props.getString(NewProp, "\uEFF1")
-	// return newText
-
-	// If in Linux, who knows. Gonna need a physical linux machine to develop this since the wlan interface isn't available in wsl
-	// But possible solution is to cat /proc/net/wireless, and if that file doesn't exist then don't show the segment
-
-	// In Windows and in wsl you can use the command `netsh.exe wlan show interfaces`. Note the .exe is important for this to work in wsl.
-
-	// if w.env.getPlatform() == windowsPlatform || w.env.isWsl() {
-	// 	output := w.env.runShellCommand(w.env.getShellName(), "netsh.exe wlan show interfaces")
-	// 	return s
-	// }
-	// return "WiFi"
-
-	s := w.env.getPlatform()
-	if w.env.isWsl() {
-		s += " isWsl"
+	segmentTemplate := w.props.getString(SegmentTemplate, "{{.SSID}} {{.SignalStrength}} {{.ReceiveRate}}Mbps")
+	template := &textTemplate{
+		Template: segmentTemplate,
+		Context:  w,
+		Env:      w.env,
 	}
-	return s
+	text, err := template.render()
+	if err != nil {
+		return err.Error()
+	}
+	return text
 }
 
 func (w *wifi) init(props *properties, env environmentInfo) {
--- src/segment_wifi_test.go
+++ src/segment_wifi_test.go
@@ -6,11 +6,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const netshOutput string = `
+There is 1 interface on the system:
+
+    Name                   : Wi-Fi
+    Description            : Intel(R) Wireless-AC 9560 160MHz
+    GUID                   : 6bb8def2-9af2-4bd4-8be2-6bd54e46bdc9
+    Physical address       : d4:3b:04:e6:10:40
+    State                  : connected
+    SSID                   : ohsiggy
+    BSSID                  : 5c:7d:7d:82:c5:73
+    Network type           : Infrastructure
+    Radio type             : 802.11ac
+    Authentication         : WPA2-Personal
+    Cipher                 : CCMP
+    Connection mode        : Profile
+    Channel                : 64
+    Receive rate (Mbps)    : 526.5
+    Transmit rate (Mbps)   : 780
+    Signal                 : 94%
+    Profile                : ohsiggy
+
+    Hosted network status  : Not available
+
+
+`
+
 func bootStrapWifiTest() *wifi {
 	env := new(MockedEnvironment)
-	//w.env.getPlatform() == windowsPlatform || w.env.isWsl()
-	env.On("getPlatform").Return(windowsPlatform)
-	env.On("isWsl").Return(false)
+	env.On("getPlatform", nil).Return(windowsPlatform)
+	env.On("isWsl", nil).Return(false)
+	env.On("runShellCommand", pwsh, "netsh.exe wlan show interfaces").Return(netshOutput)
+	env.On("getShellName", nil).Return(pwsh)
 	// env.On("hasCommand", "terraform").Return(args.hasTfCommand)
 	// env.On("hasFolder", ".terraform").Return(args.hasTfFolder)
 	// env.On("runCommand", "terraform", []string{"workspace", "show"}).Return(args.workspaceName, nil)
@@ -19,11 +46,6 @@ func bootStrapWifiTest() *wifi {
 		props: &properties{},
 	}
 	return k
-}
-
-func TestSomething(t *testing.T) {
-	wifi := bootStrapWifiTest()
-	assert.True(t, wifi.enabled())
 }
 
 func TestString(t *testing.T) {

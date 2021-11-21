93962d2f0eb8577ea4e09dd68f92056664ae7c18
Yehor Borkov <yehor.borkov@gmail.com>
2021-11-21 13:22:56 +0200

    test(palette): removed end-to-end engine test
    Instead added missing test to properties.
    This was the issue the end-to-end test allowed me to catch.

--- src/palette_colors_test.go
+++ src/palette_colors_test.go
@@ -2,33 +2,7 @@ package main
 
 import (
 	"testing"
-
-	"github.com/alecthomas/assert"
 )
-
-// FIXME: test is not completely deterministic, engineReder calls can have different
-// outputs due to time - there can be 1 second difference on the clocks during
-// rendering. The test is useful, and removing it completely will be incorrect - having
-// a close-to-real test of no palette vs palette rendering parity is better than not
-// having one. Not sure what to do with this one.
-func TestEngineRendersSamePrompt(t *testing.T) {
-	var (
-		err                    error
-		noPalette, withPalette string
-	)
-
-	noPalette, err = engineRender("jandedobbeleer.omp.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	withPalette, err = engineRender("jandedobbeleer-palette.omp.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, noPalette, withPalette)
-}
 
 func BenchmarkEngineRenderPalette(b *testing.B) {
 	var err error

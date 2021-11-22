114656521eed29b848165f0494f6bd04b331ca5b
will <billyrayleach@outlook.com>
2021-11-22 22:40:04 +0000

    refactor(winreg): use templates to handle fallback behaviour

--- docs/docs/segment-winreg.md
+++ docs/docs/segment-winreg.md
@@ -13,32 +13,25 @@ Supported registry key types:
 * String
 * DWORD (displayed in upper-case 0x hex)
 
-Additional flags are supported to configure behaviour if the key cannot be retrieved from the registry.
-
 ## Sample Configuration
 
 ```json
-{
-  "type": "winreg",
-  "style": "powerline",
-  "powerline_symbol": "\uE0B0",
-  "foreground": "#ffffff",
-  "background": "#444444",
-  "properties": {
-    "registry_path": "HKLM\\software\\microsoft\\windows nt\\currentversion",
-    "registry_key":"buildlab",
-    "query_fail_behaviour":"display_fallback_string",
-    "query_fail_fallback_string":"unknown",
-    "prefix": " \uE62A ",
-  }
-}, 
+  {
+    "type": "winreg",
+    "style": "powerline",
+    "powerline_symbol": "\uE0B0",
+    "foreground": "#ffffff",
+    "background": "#444444",
+    "properties": {
+      "registry_path": "HKLM\\software\\microsoft\\windows nt\\currentversion",
+      "registry_key":"buildlab",
+      "template":"{{if .GotKeyValue}}{{ .KeyValue }}{{else}}unknown{{ end }}",
+      "prefix": " \uE62A "
+    }
+  }, 
 ```
 
 ## Properties
 
 * registry_path: `string` - registry path to the desired key using backslashes and with a valid root HKEY name.
 * registry_key: `string` - the key to read from the registry_path location.  If "", will read the "(Default)" value.
-* query_fail_behaviour: `string` - what to do if unable to get key value from the registry for any reason:
-  * `hide_segment`: will not display this segment.
-  * `display_fallback_string`: will display the string supplied in the 'query_fail_fallback_string' property.
-* query_fail_fallback_string: `string` - display if key value could not be retrieved, per `display_fallback_string`.
--- src/segment_winreg.go
+++ src/segment_winreg.go
@@ -8,10 +8,12 @@
 package main
 
 type winreg struct {
-	props     *properties
-	env       environmentInfo
-	content   string
-	errorInfo string
+	props         *properties
+	env           environmentInfo
+	segmentString string
+
+	KeyValue    string
+	GotKeyValue bool
 }
 
 const (
@@ -19,51 +21,59 @@ const (
 	// Properties defining how to get to the key.
 	RegistryPath Property = "registry_path" // path from the supplied root under which the key exists
 	RegistryKey  Property = "registry_key"  // key within full reg path formed from two above
-
-	// Properties defining fallback behaviour in case the query fails.
-	QueryFailBehaviour      Property = "query_fail_behaviour"       // what to do, can be one of "hide_segment", "display_fallback_string", "show_debug_info"
-	QueryFailFallbackString Property = "query_fail_fallback_string" // what to display if above is "display_fallback_string"
 )
 
-func (r *winreg) string() string {
-	newText := r.content
-	return newText
+func (wr *winreg) init(props *properties, env environmentInfo) {
+	wr.props = props
+	wr.env = env
+	wr.KeyValue = ""
 }
 
-func (r *winreg) init(props *properties, env environmentInfo) {
-	r.props = props
-	r.env = env
-}
-
-func (r *winreg) enabled() bool {
+func (wr *winreg) enabled() bool {
 	// Definitely no point going further if it's not windows.
-	if r.env.getRuntimeGOOS() != windowsPlatform {
+	if wr.env.getRuntimeGOOS() != windowsPlatform {
 		return false
 	}
 
-	var enableSegment bool = false
+	//var enableSegment bool = false
 
-	registryPath := r.props.getString(RegistryPath, "")
-	registryKey := r.props.getString(RegistryKey, "")
+	registryPath := wr.props.getString(RegistryPath, "")
+	registryKey := wr.props.getString(RegistryKey, "")
 
-	regValue, err := r.env.getWindowsRegistryKeyValue(registryPath, registryKey)
+	keyValue, err := wr.env.getWindowsRegistryKeyValue(registryPath, registryKey)
 
-	// Fallback behaviour
-	failBehaviour := r.props.getString(QueryFailBehaviour, "hide_segment")
-	fallbackString := r.props.getString(QueryFailFallbackString, "")
+	wr.KeyValue = keyValue
+	wr.GotKeyValue = (err == nil)
 
-	if err != nil {
-		switch failBehaviour {
-		case "hide_segment":
-			enableSegment = false
-		case "display_fallback_string":
-			r.content = fallbackString
-			enableSegment = true
+	// Need to do the template work here, not in string() to determine whether to display the segment
+	// If there is a template and the resulting output is not empty, then enable the segment.
+	segmentTemplate := wr.props.getString(SegmentTemplate, "")
+	if len(segmentTemplate) > 0 {
+		displayString := wr.templateString(segmentTemplate)
+		if len(displayString) > 0 {
+			wr.segmentString = displayString
+			return true
 		}
-	} else {
-		r.content = regValue
-		enableSegment = true
 	}
 
-	return enableSegment
+	// failing that, enabled if we got a key value
+	wr.segmentString = wr.KeyValue
+	return wr.GotKeyValue
+}
+
+func (wr *winreg) string() string {
+	return wr.segmentString
+}
+
+func (wr *winreg) templateString(segmentTemplate string) string {
+	template := &textTemplate{
+		Template: segmentTemplate,
+		Context:  wr,
+		Env:      wr.env,
+	}
+	text, err := template.render()
+	if err != nil {
+		return err.Error()
+	}
+	return text
 }

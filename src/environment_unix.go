1afe311575d4d3ba4268c47971129bd9b8b7b756
William Leach <wleach@microsoft.com>
2021-11-23 11:25:45 +0000

    fix(winreg): lint errors in interface and unix impl

--- src/environment_unix.go
+++ src/environment_unix.go
@@ -60,6 +60,6 @@ func (env *environment) getCachePath() string {
 	return env.homeDir()
 }
 
-func (env *environment) getWindowsRegistryKeyValue(regPath string, regKey string) (string, error) {
+func (env *environment) getWindowsRegistryKeyValue(regPath, regKey string) (string, error) {
 	return "", errors.New("not implemented")
 }

5256c90531a07b6404fb3f8e0fa1f2ae81e92be2
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-11-26 12:41:33 +0100

    chore: a better PowerShell experience

--- .devcontainer/Microsoft.PowerShell_profile.ps1
+++ .devcontainer/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,11 @@
+Import-Module posh-git
+Import-Module PSFzf -ArgumentList 'Ctrl+t', 'Ctrl+r'
+Import-Module z
+Import-Module Terminal-Icons
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+$env:POSH_GIT_ENABLED=$true
 oh-my-posh --init --shell pwsh --config $env:POSH_THEME | Invoke-Expression
 
 # NOTE: You can override the above env var from the devcontainer.json "args" under the "build" key.

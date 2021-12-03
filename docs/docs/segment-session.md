36f8d9e5cd89b28975491c1aa69a239a168f0cf1
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-12-03 23:44:58 +0100

    feat(session): refactor for template

--- docs/docs/segment-session.md
+++ docs/docs/segment-session.md
@@ -17,37 +17,26 @@ Show the current user and host name.
   "foreground": "#ffffff",
   "background": "#c386f1",
   "leading_diamond": "\uE0B6",
-  "trailing_diamond": "\uE0B0"
+  "trailing_diamond": "\uE0B0",
+  "properties": {
+    "template": "{{ .UserName }}"
+  }
 }
 ```
 
 ## Properties
 
-- user_info_separator: `string` - text/icon to put in between the user and host name - defaults to `@`
 - ssh_icon: `string` - text/icon to display first when in an active SSH session - defaults
 to `\uF817 `
-- user_color: `string` [color][colors] - override the foreground color of the user name
-- host_color: `string` [color][colors] - override the foreground color of the host name
-- display_user: `boolean` - display the user name or not - defaults to `true`
-- display_host: `boolean` - display the host name or not - defaults to `true`
-- default_user_name: `string` - name of the default user - defaults to empty
-- display_default: `boolean` - display the segment or not when the user matches `default_user_name` - defaults
-to `true`
 - template: `string` - A go [text/template][go-text-template] template extended with [sprig][sprig] utilizing the
-properties below. Only used when a value is set, making the above properties obsolete.
+properties below.
 
 ## Template Properties
 
 - `.UserName`: `string` - the current user's name
-- `.DefaultUserName`: - the default user name (set with the `POSH_SESSION_DEFAULT_USER` env var or `default_user_name` property)
 - `.ComputerName`: `string` - the current computer's name
 - `.SSHSession`: `boolean` - active SSH session or not
 - `.Root`: `boolean` - are you a root/admin user or not
 
-## Environmnent Variables
-
-- `POSH_SESSION_DEFAULT_USER` - used to override the hardcoded `default_user_name` property
-
-[colors]: /docs/config-colors
 [go-text-template]: https://golang.org/pkg/text/template/
 [sprig]: https://masterminds.github.io/sprig/
--- src/segment_session.go
+++ src/segment_session.go
@@ -1,68 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 type session struct {
-	props           properties
-	env             environmentInfo
-	UserName        string
+	props properties
+	env   environmentInfo
+	text  string
+
+	UserName     string
+	ComputerName string
+	SSHSession   bool
+	Root         bool
+
 	DefaultUserName string
-	ComputerName    string
-	SSHSession      bool
-	Root            bool
-	templateText    string
 }
 
 const (
-	// UserInfoSeparator is put between the user and computer name
-	UserInfoSeparator Property = "user_info_separator"
-	// UserColor if set, is used to color the user name
-	UserColor Property = "user_color"
-	// HostColor if set, is used to color the computer name
-	HostColor Property = "host_color"
-	// DisplayHost hides or show the computer name
-	DisplayHost Property = "display_host"
-	// DisplayUser hides or shows the user name
-	DisplayUser Property = "display_user"
-	// SSHIcon shows when in an SSH session
+	// SSHIcon is the icon used for SSH sessions
 	SSHIcon Property = "ssh_icon"
-	// DefaultUserName holds the default user of the platform
-	DefaultUserName Property = "default_user_name"
-
-	defaultUserEnvVar = "POSH_SESSION_DEFAULT_USER"
 )
 
 func (s *session) enabled() bool {
+	s.SSHSession = s.activeSSHSession()
+	segmentTemplate := s.props.getString(SegmentTemplate, "")
+	if segmentTemplate == "" {
+		return s.legacyEnabled()
+	}
 	s.UserName = s.getUserName()
 	s.ComputerName = s.getComputerName()
-	s.SSHSession = s.activeSSHSession()
-	s.DefaultUserName = s.getDefaultUser()
-	segmentTemplate := s.props.getString(SegmentTemplate, "")
-	if segmentTemplate != "" {
-		s.Root = s.env.isRunningAsRoot()
-		template := &textTemplate{
-			Template: segmentTemplate,
-			Context:  s,
-			Env:      s.env,
-		}
-		var err error
-		s.templateText, err = template.render()
-		if err != nil {
-			s.templateText = err.Error()
-		}
-		return len(s.templateText) > 0
+	s.Root = s.env.isRunningAsRoot()
+	template := &textTemplate{
+		Template: segmentTemplate,
+		Context:  s,
+		Env:      s.env,
 	}
-	showDefaultUser := s.props.getBool(DisplayDefault, true)
-	if !showDefaultUser && s.DefaultUserName == s.UserName {
-		return false
+	var err error
+	s.text, err = template.render()
+	if err != nil {
+		s.text = err.Error()
 	}
-	return true
+	return len(s.text) > 0
 }
 
 func (s *session) string() string {
+	if len(s.text) != 0 {
+		return s.text
+	}
 	return s.getFormattedText()
 }
 
@@ -71,48 +54,7 @@ func (s *session) init(props properties, env environmentInfo) {
 	s.env = env
 }
 
-func (s *session) getFormattedText() string {
-	if len(s.templateText) > 0 {
-		return s.templateText
-	}
-	separator := ""
-	if s.props.getBool(DisplayHost, true) && s.props.getBool(DisplayUser, true) {
-		separator = s.props.getString(UserInfoSeparator, "@")
-	}
-	var sshIcon string
-	if s.SSHSession {
-		sshIcon = s.props.getString(SSHIcon, "\uF817 ")
-	}
-	defaulColor := s.props.getColor(ForegroundOverride, "")
-	userColor := s.props.getColor(UserColor, defaulColor)
-	hostColor := s.props.getColor(HostColor, defaulColor)
-	if len(userColor) > 0 && len(hostColor) > 0 {
-		return fmt.Sprintf("%s<%s>%s</>%s<%s>%s</>", sshIcon, userColor, s.UserName, separator, hostColor, s.ComputerName)
-	}
-	if len(userColor) > 0 {
-		return fmt.Sprintf("%s<%s>%s</>%s%s", sshIcon, userColor, s.UserName, separator, s.ComputerName)
-	}
-	if len(hostColor) > 0 {
-		return fmt.Sprintf("%s%s%s<%s>%s</>", sshIcon, s.UserName, separator, hostColor, s.ComputerName)
-	}
-	return fmt.Sprintf("%s%s%s%s", sshIcon, s.UserName, separator, s.ComputerName)
-}
-
-func (s *session) getComputerName() string {
-	if !s.props.getBool(DisplayHost, true) {
-		return ""
-	}
-	computername, err := s.env.getHostName()
-	if err != nil {
-		computername = "unknown"
-	}
-	return strings.TrimSpace(computername)
-}
-
 func (s *session) getUserName() string {
-	if !s.props.getBool(DisplayUser, true) {
-		return ""
-	}
 	user := s.env.getCurrentUser()
 	username := strings.TrimSpace(user)
 	if s.env.getRuntimeGOOS() == "windows" && strings.Contains(username, "\\") {
@@ -121,12 +63,12 @@ func (s *session) getUserName() string {
 	return username
 }
 
-func (s *session) getDefaultUser() string {
-	user := s.env.getenv(defaultUserEnvVar)
-	if len(user) == 0 {
-		user = s.props.getString(DefaultUserName, "")
+func (s *session) getComputerName() string {
+	computername, err := s.env.getHostName()
+	if err != nil {
+		computername = "unknown"
 	}
-	return user
+	return strings.TrimSpace(computername)
 }
 
 func (s *session) activeSSHSession() bool {

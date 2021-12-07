d3c3dea1d76ce0edc4ae834989929351762a0137
lnu <l.nullens@gmail.com>
2021-12-07 06:27:28 +0000

    feat(language): parameter for version detection
    applied to angular segment to read version from package.json

--- src/segment_angular.go
+++ src/segment_angular.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type angular struct {
 	language
 }
@@ -15,9 +20,28 @@ func (a *angular) init(props properties, env environmentInfo) {
 		extensions: []string{"angular.json"},
 		commands: []*cmd{
 			{
-				executable: "ng",
-				args:       []string{"--version"},
-				regex:      `Angular CLI: (?:(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+).(?P<patch>[0-9]+))))`,
+				regex: `(?:(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+).(?P<patch>[0-9]+))))`,
+				getVersion: func() (string, error) {
+					const fileName string = "package.json"
+					const fileFolder string = "/node_modules/@angular/core"
+					angularFilePath := a.language.env.getcwd() + fileFolder
+					if !a.language.env.hasFilesInDir(angularFilePath, fileName) {
+						return "", fmt.Errorf("%s not found in %s", fileName, angularFilePath)
+					}
+					// parse file
+					objmap := map[string]json.RawMessage{}
+					content := a.language.env.getFileContent(a.language.env.getcwd() + fileFolder + "/" + fileName)
+					err := json.Unmarshal([]byte(content), &objmap)
+					if err != nil {
+						return "", err
+					}
+					var str string
+					err = json.Unmarshal(objmap["version"], &str)
+					if err != nil {
+						return "", err
+					}
+					return str, nil
+				},
 			},
 		},
 		versionURLTemplate: "[%s](https://github.com/angular/angular/releases/tag/%s.%s.%s)",

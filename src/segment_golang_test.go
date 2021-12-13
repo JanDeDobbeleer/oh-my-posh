89ec830c0d7ba5d6ed232d4ea08d535be021dfd3
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-12-13 22:35:42 +0100

    feat(go): parse go.mod file for version

--- src/segment_golang_test.go
+++ src/segment_golang_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -29,12 +31,25 @@ func getMockedLanguageEnv(params *mockedLanguageParams) (*MockedEnvironment, pro
 
 func TestGolang(t *testing.T) {
 	cases := []struct {
-		Case           string
-		ExpectedString string
-		Version        string
+		Case                  string
+		ExpectedString        string
+		Version               string
+		ParseModFile          bool
+		HasModFileInParentDir bool
+		InvalidModfile        bool
 	}{
 		{Case: "Go 1.15", ExpectedString: "1.15.8", Version: "go version go1.15.8 darwin/amd64"},
 		{Case: "Go 1.16", ExpectedString: "1.16", Version: "go version go1.16 darwin/amd64"},
+		{Case: "go.mod 1.17", ParseModFile: true, HasModFileInParentDir: true, ExpectedString: "1.17"},
+		{Case: "no go.mod file fallback", ParseModFile: true, ExpectedString: "1.16", Version: "go version go1.16 darwin/amd64"},
+		{
+			Case:                  "invalid go.mod file fallback",
+			ParseModFile:          true,
+			HasModFileInParentDir: true,
+			InvalidModfile:        true,
+			ExpectedString:        "./go.mod:1: unknown directive: invalid",
+			Version:               "go version go1.16 darwin/amd64",
+		},
 	}
 	for _, tc := range cases {
 		params := &mockedLanguageParams{
@@ -44,6 +59,27 @@ func TestGolang(t *testing.T) {
 			extension:     "*.go",
 		}
 		env, props := getMockedLanguageEnv(params)
+		if tc.ParseModFile {
+			props[ParseModFile] = tc.ParseModFile
+			fileInfo := &fileInfo{
+				path:         "./go.mod",
+				parentFolder: "./",
+				isDir:        false,
+			}
+			var err error
+			if !tc.HasModFileInParentDir {
+				err = errors.New("no match")
+			}
+			env.On("hasParentFilePath", "go.mod").Return(fileInfo, err)
+			var content string
+			if tc.InvalidModfile {
+				content = "invalid go.mod file"
+			} else {
+				tmp, _ := ioutil.ReadFile(fileInfo.path)
+				content = string(tmp)
+			}
+			env.On("getFileContent", fileInfo.path).Return(content)
+		}
 		g := &golang{}
 		g.init(props, env)
 		assert.True(t, g.enabled(), fmt.Sprintf("Failed in case: %s", tc.Case))

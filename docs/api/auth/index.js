2c35779900e8e00d16c13e029731f0de67b5102d
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2022-01-04 09:39:45 +0100

    feat: preliminary auth for strava

--- docs/api/auth/index.js
+++ docs/api/auth/index.js
@@ -1,13 +1,21 @@
+const axios = require('axios').default;
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
+  // http://www.strava.com/oauth/authorize?client_id=76033&response_type=code&redirect_uri=https://ohmyposh.dev/api/auth&approval_prompt=force&scope=read
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const code = (req.query.code || (req.body && req.body.code));
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  var data = {
+    client_id: process.env.STRAVA_CLIENT_ID,
+    client_secret: process.env.STRAVA_CLIENT_SECRET,
+    code: code,
+    grant_type: 'authorization_code',
+  };
+  const resp = await axios.post('https://www.strava.com/oauth/token', data);
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: resp.data
+  };
 }

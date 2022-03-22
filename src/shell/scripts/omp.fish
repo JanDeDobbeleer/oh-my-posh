340c8517436343212e16aec3db88e9a3652c5b9c
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2022-03-22 19:53:37 +0100

    feat(fish): tooltip support

--- src/shell/scripts/omp.fish
+++ src/shell/scripts/omp.fish
@@ -1,6 +1,7 @@
 set -g POSH_THEME "::CONFIG::"
 set -g POWERLINE_COMMAND "oh-my-posh"
 set -g CONDA_PROMPT_MODIFIER false
+set -g omp_tooltip_command ""
 
 function fish_prompt
     set -g omp_status_cache $status
@@ -22,6 +23,11 @@ function fish_prompt
 end
 
 function fish_right_prompt
+    if test -n "$omp_tooltip_command"
+      ::OMP:: prompt print tooltip --config $POSH_THEME --shell fish --command $omp_tooltip_command
+      set omp_tooltip_command ""
+      return
+    end
     ::OMP:: prompt print right --config $POSH_THEME --shell fish --error $omp_status_cache --execution-time $omp_duration --stack-count $omp_stack_count
 end
 
@@ -29,4 +35,16 @@ function postexec_omp --on-event fish_postexec
   # works with fish <3.2
   # pre and postexec not fired for empty command in fish >=3.2
   set -gx omp_lastcommand $argv
+end
+
+# tooltip
+
+function _render_tooltip
+  set omp_tooltip_command (commandline --current-buffer | string collect)
+  commandline --insert " "
+  commandline --function repaint
+end
+
+function enable_poshtooltips
+  bind \x20 _render_tooltip
 end

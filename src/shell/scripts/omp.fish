9b437b6956905eb8a4a7eeaa4fb42d2b6bdea2e0
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2022-03-27 09:32:12 +0200

    fix(fish): export POSH_THEME as env var

--- src/shell/scripts/omp.fish
+++ src/shell/scripts/omp.fish
@@ -1,4 +1,4 @@
-set --global POSH_THEME "::CONFIG::"
+set --export POSH_THEME "::CONFIG::"
 set --global POWERLINE_COMMAND "oh-my-posh"
 set --global CONDA_PROMPT_MODIFIER false
 set --global omp_tooltip_command ""

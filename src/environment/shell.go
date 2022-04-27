1420fd59239a7a1e34c0421d82203e5fad65f839
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2022-04-27 21:24:31 +0200

    fix: recover crashing function
    relates to #2058

--- src/environment/shell.go
+++ src/environment/shell.go
@@ -711,8 +711,21 @@ func (env *ShellEnvironment) TemplateCache() *TemplateCache {
 	return tmplCache
 }
 
-func (env *ShellEnvironment) DirMatchesOneOf(dir string, regexes []string) bool {
-	return dirMatchesOneOf(dir, env.Home(), env.GOOS(), regexes)
+func (env *ShellEnvironment) DirMatchesOneOf(dir string, regexes []string) (match bool) {
+	// sometimes the function panics inside golang, we want to silence that error
+	// and assume that there's no match. Not perfect, but better than crashing
+	// for the time being until we figure out what the actual root cause is
+	defer func() {
+		if err := recover(); err != nil {
+			message := fmt.Sprintf("%s", err)
+			env.log(Error, "DirMatchesOneOf", message)
+			match = false
+		}
+	}()
+	env.lock.Lock()
+	defer env.lock.Unlock()
+	match = dirMatchesOneOf(dir, env.Home(), env.GOOS(), regexes)
+	return
 }
 
 func dirMatchesOneOf(dir, home, goos string, regexes []string) bool {

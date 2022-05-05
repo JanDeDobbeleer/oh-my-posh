2e5fa3f1d29c966b77166440d5fe278ebe5d9342
L. Yeung <lewis_yeung-ly@outlook.com>
2022-05-06 00:06:31 +0800

    feat(image): add `-o, --output` flag

--- src/cli/config_export_image.go
+++ src/cli/config_export_image.go
@@ -16,6 +16,7 @@ var (
 	cursorPadding int
 	rPromptOffset int
 	bgColor       string
+	outputImage   string
 )
 
 // imageCmd represents the image command
@@ -36,6 +37,10 @@ Example usage:
 > oh-my-posh config export image --config ~/myconfig.omp.json
 
 Exports the config to an image file called myconfig.png in the current working directory.
+
+> oh-my-posh config export image --config ~/myconfig.omp.json --output ~/mytheme.png
+
+Exports the config to an image file ~/mytheme.png.
 
 > oh-my-posh config export image --config ~/myconfig.omp.json --author "John Doe"
 
@@ -81,6 +86,9 @@ Exports the config to an image file using customized output options.`,
 			BgColor:       bgColor,
 			Ansi:          ansi,
 		}
+		if outputImage != "" {
+			imageCreator.Path = cleanOutputPath(outputImage, env)
+		}
 		imageCreator.Init(env.Flags().Config)
 		err := imageCreator.SavePNG()
 		if err != nil {
@@ -94,5 +102,6 @@ func init() { // nolint:gochecknoinits
 	imageCmd.Flags().StringVar(&bgColor, "background-color", "", "image background color")
 	imageCmd.Flags().IntVar(&cursorPadding, "cursor-padding", 0, "prompt cursor padding")
 	imageCmd.Flags().IntVar(&rPromptOffset, "rprompt-offset", 0, "right prompt offset")
+	imageCmd.Flags().StringVarP(&outputImage, "output", "o", "", "image file (.png) to export to")
 	exportCmd.AddCommand(imageCmd)
 }

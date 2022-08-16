32f1977bdc04d0e8f217646d137ad897851d1441
Jan De Dobbeleer <2492783+JanDeDobbeleer@users.noreply.github.com>
2022-08-16 20:55:08 +0200

    feat(wakatime): parse url as template

--- src/segments/wakatime_test.go
+++ src/segments/wakatime_test.go
@@ -3,11 +3,13 @@ package segments
 import (
 	"errors"
 	"fmt"
+	"oh-my-posh/environment"
 	"oh-my-posh/mock"
 	"oh-my-posh/properties"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	mock2 "github.com/stretchr/testify/mock"
 )
 
 func TestWTTrackedTime(t *testing.T) {
@@ -80,9 +82,12 @@ func TestWTTrackedTime(t *testing.T) {
 		cache.On("Set", FAKEAPIURL, response, tc.CacheTimeout).Return()
 		env.On("Cache").Return(cache)
 
+		env.On("TemplateCache").Return(&environment.TemplateCache{
+			Env: map[string]string{"HELLO": "hello"},
+		})
+
 		w := &Wakatime{
 			props: properties.Map{
-				APIKey:                  "key",
 				properties.CacheTimeout: tc.CacheTimeout,
 				URL:                     FAKEAPIURL,
 			},
@@ -91,5 +96,54 @@ func TestWTTrackedTime(t *testing.T) {
 
 		assert.ErrorIs(t, tc.Error, w.setAPIData(), tc.Case+" - Error")
 		assert.Equal(t, tc.Expected, renderTemplate(env, w.Template(), w), tc.Case+" - String")
+	}
+}
+
+func TestWTGetUrl(t *testing.T) {
+	cases := []struct {
+		Case        string
+		Expected    string
+		URL         string
+		ShouldError bool
+	}{
+		{
+			Case:     "no template",
+			Expected: "test",
+			URL:      "test",
+		},
+		{
+			Case:     "template",
+			URL:      "{{ .Env.HELLO }} world",
+			Expected: "hello world",
+		},
+		{
+			Case:        "error",
+			URL:         "{{ .BURR }}",
+			ShouldError: true,
+		},
+	}
+
+	for _, tc := range cases {
+		env := &mock.MockedEnvironment{}
+
+		env.On("Log", mock2.Anything, mock2.Anything, mock2.Anything)
+		env.On("TemplateCache").Return(&environment.TemplateCache{
+			Env: map[string]string{"HELLO": "hello"},
+		})
+
+		w := &Wakatime{
+			props: properties.Map{
+				URL: tc.URL,
+			},
+			env: env,
+		}
+
+		got, err := w.getURL()
+
+		if tc.ShouldError {
+			assert.Error(t, err, tc.Case)
+			continue
+		}
+		assert.Equal(t, tc.Expected, got, tc.Case)
 	}
 }

3c5629d20cecda98a1a9a082042ca314af95caad
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2022-11-12 17:53:57 +0100

    fix: add space after link text

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -93,7 +93,7 @@ module.exports = {
               href: 'https://github.com/jandedobbeleer/oh-my-posh',
             },
             {
-              html: "<a class=\"footer__link-item\" rel=\"me\" href=\"https://social.ohmyposh.dev/@jan\">Mastodon<svg width=\"13.5\" height=\"13.5\" aria-hidden=\"true\" viewBox=\"0 0 24 24\" class=\"iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module\"><path fill=\"currentColor\" d=\"M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z\"></path></svg></a>",
+              html: "<a class=\"footer__link-item\" rel=\"me\" href=\"https://social.ohmyposh.dev/@jan\">Mastodon <svg width=\"13.5\" height=\"13.5\" aria-hidden=\"true\" viewBox=\"0 0 24 24\" class=\"iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module\"><path fill=\"currentColor\" d=\"M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z\"></path></svg></a>",
             },
           ],
         },

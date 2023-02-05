9b1eed05e8fd1d9a8a1a63722f23917da73fe268
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2023-02-05 21:30:10 +0100

    fix(fish): do not use the iTerm segment

--- src/segments/iterm.go
+++ src/segments/iterm.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jandedobbeleer/oh-my-posh/src/platform"
 	"github.com/jandedobbeleer/oh-my-posh/src/properties"
+	"github.com/jandedobbeleer/oh-my-posh/src/shell"
 )
 
 type ITerm struct {
@@ -21,6 +22,7 @@ func (i *ITerm) Template() string {
 func (i *ITerm) Enabled() bool {
 	promptMark, err := i.getResult()
 	if err != nil {
+		i.env.Error(err)
 		return false
 	}
 	i.PromptMark = promptMark
@@ -35,19 +37,17 @@ func (i *ITerm) getResult() (string, error) {
 		return "", errors.New("Only works with iTerm")
 	}
 
-	// Check to ensure the user has squelched the default mark
+	// Check to ensure the user has squelched the default mark for BASH and ZSH
 	if i.env.Getenv("ITERM2_SQUELCH_MARK") != "1" {
-		return "", errors.New("iTerm default mark enabled (set ITERM2_SQUELCH_MARK=1)")
+		return "", errors.New("iTerm default mark enabled (export ITERM2_SQUELCH_MARK=1)")
 	}
 
 	// Now, set the mark string based on shell (or error out)
 	switch i.env.Shell() {
-	case "zsh":
+	case shell.ZSH:
 		response = `%{$(iterm2_prompt_mark)%}`
-	case "bash":
+	case shell.BASH:
 		response = `\[$(iterm2_prompt_mark)\]`
-	case "fish":
-		response = `iterm2_prompt_mark`
 	default:
 		return "", errors.New("Shell isn't compatible with iTerm Shell Integration")
 	}

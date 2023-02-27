3cd303e35e1a651b588047e6aec69c1b7f69710b
cabauman <cab452005@gmail.com>
2023-02-27 13:16:20 +0000

    fix(unity): output corrupt with CRLF line ending

--- src/segments/unity.go
+++ src/segments/unity.go
@@ -64,6 +64,7 @@ func (u *Unity) GetUnityVersion() (version string, err error) {
 
 	versionStartIndex := versionPrefixIndex + len(versionPrefix)
 	unityVersion := firstLine[versionStartIndex:]
+	unityVersion = strings.TrimSuffix(unityVersion, "\r")
 
 	return strings.TrimSuffix(unityVersion, "f1"), nil
 }

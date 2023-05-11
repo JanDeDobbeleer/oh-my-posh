c12d6b50dc3905d9830705fcf2e44a8ad988a51d
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2023-05-11 17:19:12 +0200

    chore(install): correct arch for amd64
    resolves #3838

--- website/static/install.sh
+++ website/static/install.sh
@@ -128,7 +128,7 @@ detect_arch() {
   arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
 
   case "${arch}" in
-    amd64) arch="386" ;;
+    x86_64) arch="amd64" ;;
     armv*) arch="arm" ;;
     arm64) arch="arm64" ;;
   esac

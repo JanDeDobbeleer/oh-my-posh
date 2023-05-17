c62a272e72046b6e0384346c5ce7bf305efedd8d
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2023-05-17 20:03:57 +0200

    chore(tests): do not fail test on no connection
    resolves #3860

--- src/engine/image_test.go
+++ src/engine/image_test.go
@@ -48,17 +48,26 @@ func runImageTest(config, content string) (string, error) {
 		},
 	}
 
-	_ = image.Init(env)
+	err = image.Init(env)
+	if err != nil {
+		return "", err
+	}
+
 	err = image.SavePNG()
 	if err == nil {
 		os.Remove(image.Path)
 	}
+
 	return filepath.Base(image.Path), err
 }
 
 func TestStringImageFileWithText(t *testing.T) {
 	for _, tc := range cases {
 		filename, err := runImageTest(tc.Config, "foobar")
+		if connectionError, ok := err.(*ConnectionError); ok {
+			t.Log(connectionError.Error())
+			continue
+		}
 		assert.Equal(t, "jandedobbeleer.png", filename, tc.Case)
 		assert.NoError(t, err)
 	}
@@ -68,6 +77,10 @@ func TestStringImageFileWithANSI(t *testing.T) {
 	prompt := `[38;2;40;105;131m[0m[48;2;40;105;131m[38;2;224;222;244m jan [0m[38;2;40;105;131m[0m[38;2;224;222;244m [0m`
 	for _, tc := range cases {
 		filename, err := runImageTest(tc.Config, prompt)
+		if connectionError, ok := err.(*ConnectionError); ok {
+			t.Log(connectionError.Error())
+			continue
+		}
 		assert.Equal(t, "jandedobbeleer.png", filename, tc.Case)
 		assert.NoError(t, err)
 	}

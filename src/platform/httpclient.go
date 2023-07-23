ecf71ede2a873f64c0232c9d99c78fc657820921
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2023-07-23 09:28:55 +0200

    fix(http): only use HTTPS_PROXY

--- src/platform/httpclient.go
+++ src/platform/httpclient.go
@@ -3,6 +3,8 @@ package platform
 import (
 	"net"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 )
 
@@ -12,9 +14,17 @@ type httpClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+func Proxy(_ *http.Request) (*url.URL, error) {
+	proxyURL := os.Getenv("HTTPS_PROXY")
+	if len(proxyURL) == 0 {
+		return nil, nil
+	}
+	return url.Parse(proxyURL)
+}
+
 var (
 	defaultTransport http.RoundTripper = &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
+		Proxy: Proxy,
 		Dial: (&net.Dialer{
 			Timeout: 10 * time.Second,
 		}).Dial,

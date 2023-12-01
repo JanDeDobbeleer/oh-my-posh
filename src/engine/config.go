e9a3dd491fcc92b5b8371f93eb3611c5e067ea92
Jan De Dobbeleer <2492783+JanDeDobbeleer@users.noreply.github.com>
2023-12-01 11:20:40 +0100

    fix(config): update default to use STATUS segment

--- src/engine/config.go
+++ src/engine/config.go
@@ -404,7 +404,7 @@ func defaultConfig(env platform.Environment, warning bool) *Config {
 						Template:        " \uf0e7 ",
 					},
 					{
-						Type:            EXIT,
+						Type:            STATUS,
 						Style:           Diamond,
 						LeadingDiamond:  "<transparent,background>\ue0b0</>",
 						TrailingDiamond: "\ue0b4",

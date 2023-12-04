5493ddb640877ff1fa3d6d640664a5eeb161c4c2
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2023-12-04 19:41:58 +0100

    feat(template): add readFile
    closes #4518

--- src/template/files.go
+++ src/template/files.go
@@ -1,6 +1,9 @@
 package template
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+)
 
 func glob(pattern string) (bool, error) {
 	matches, err := filepath.Glob(pattern)
@@ -8,4 +11,9 @@ func glob(pattern string) (bool, error) {
 		return false, err
 	}
 	return len(matches) > 0, nil
+}
+
+func readFile(path string) string {
+	content, _ := os.ReadFile(path)
+	return string(content)
 }

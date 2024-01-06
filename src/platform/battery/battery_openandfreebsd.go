e17ee69028a38adf01ce8f0eb169245d35095ba1
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-01-06 08:41:13 +0100

    feat: build for freebsd
    resolves #4585

--- src/platform/battery/battery_openandfreebsd.go
+++ src/platform/battery/battery_openandfreebsd.go
@@ -1,3 +1,5 @@
+//go:build openbsd || freebsd
+
 package battery
 
 import (
--- src/platform/battery/battery_windows_nix.go
+++ src/platform/battery/battery_windows_nix.go
@@ -1,4 +1,4 @@
-//go:build !darwin && !netbsd && !openbsd
+//go:build !darwin && !netbsd && !openbsd && !freebsd
 
 package battery
 

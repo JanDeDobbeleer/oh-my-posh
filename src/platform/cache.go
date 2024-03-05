dceea415acf6ec06b1ae61722c038838b7db232f
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-03-05 08:43:56 +0100

    refactor: move cache out of shell

--- src/platform/cache.go
+++ src/platform/cache.go
@@ -2,13 +2,44 @@ package platform
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"sync"
 	"time"
 )
 
+type Cache interface {
+	Init(home string)
+	Close()
+	// Gets the value for a given key.
+	// Returns the value and a boolean indicating if the key was found.
+	// In case the ttl expired, the function returns false.
+	Get(key string) (string, bool)
+	// Sets a value for a given key.
+	// The ttl indicates how many minutes to cache the value.
+	Set(key, value string, ttl int)
+	// Deletes a key from the cache.
+	Delete(key string)
+}
+
 const (
 	CacheFile = "/omp.cache"
+)
+
+func pid() string {
+	pid := os.Getenv("POSH_PID")
+	if len(pid) == 0 {
+		pid = strconv.Itoa(os.Getppid())
+	}
+	return pid
+}
+
+var (
+	TEMPLATECACHE    = fmt.Sprintf("template_cache_%s", pid())
+	TOGGLECACHE      = fmt.Sprintf("toggle_cache_%s", pid())
+	PROMPTCOUNTCACHE = fmt.Sprintf("prompt_count_cache_%s", pid())
 )
 
 type cacheObject struct {
@@ -90,4 +121,51 @@ func (fc *fileCache) Set(key, value string, ttl int) {
 func (fc *fileCache) Delete(key string) {
 	fc.cache.Delete(key)
 	fc.dirty = true
+}
+
+type commandCache struct {
+	commands *ConcurrentMap
+}
+
+func (c *commandCache) set(command, path string) {
+	c.commands.Set(command, path)
+}
+
+func (c *commandCache) get(command string) (string, bool) {
+	cacheCommand, found := c.commands.Get(command)
+	if !found {
+		return "", false
+	}
+	command, ok := cacheCommand.(string)
+	return command, ok
+}
+
+type TemplateCache struct {
+	Root          bool
+	PWD           string
+	Folder        string
+	Shell         string
+	ShellVersion  string
+	UserName      string
+	HostName      string
+	Code          int
+	Env           map[string]string
+	Var           SimpleMap
+	OS            string
+	WSL           bool
+	PromptCount   int
+	SHLVL         int
+	Segments      *ConcurrentMap
+	SegmentsCache SimpleMap
+
+	initialized bool
+	sync.RWMutex
+}
+
+func (t *TemplateCache) AddSegmentData(key string, value any) {
+	t.Segments.Set(key, value)
+}
+
+func (t *TemplateCache) RemoveSegmentData(key string) {
+	t.Segments.Delete(key)
 }

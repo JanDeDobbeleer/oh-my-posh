902c9850cd2445345f1f8ba20ff7cc31d2d284ac
luisegarduno <luis.e.garduno@gmail.com>
2024-03-20 13:46:39 -0400

    - Changed: RunShellCommand() --> RunCommand()

--- src/segments/spotify_linux.go
+++ src/segments/spotify_linux.go
@@ -3,20 +3,45 @@
 package segments
 
 import (
+	"encoding/json"
 	"strings"
-
-	"github.com/jandedobbeleer/oh-my-posh/src/shell"
 )
 
+type Metadata struct {
+	TrackID     string   `json:"mpris:trackid"`
+	Length      uint64   `json:"mpris:length"`
+	ArtURL      string   `json:"mpris:artUrl"`
+	Album       string   `json:"xesam:album"`
+	AlbumArtist []string `json:"xesam:albumArtist"`
+	Artist      []string `json:"xesam:artist"`
+	AutoRating  float64  `json:"xesam:autoRating"`
+	DiscNumber  int32    `json:"xesam:discNumber"`
+	Title       string   `json:"xesam:title"`
+	TrackNumber int32    `json:"xesam:trackNumber"`
+	URL         string   `json:"xesam:url"`
+}
+
 func (s *Spotify) Enable() bool {
-	command := "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata"
-	tlist := s.env.RunShellCommand(shell.BASH, command)
 
-	infos := strings.Split(tlist, " - ")
-	s.Status = playing
-	s.Artist = infos[0]
-	s.Track = infos[1]
-	s.resolveIcon()
+	tlist, _ := s.env.RunCommand("dbus-send", "--print-reply", "--dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata")
 
-	return true
+	// Split the output into lines
+	lines := strings.Split(tlist, "\n")
+
+	// Extract the JSON-formatted data
+	var jsonData string
+	for _, line := range lines {
+		if strings.Contains(line, "variant") {
+			jsonData = strings.TrimSpace(line[len("   variant       array ["):])
+			break
+		}
+	}
+
+	var metadata []Metadata
+	// Parsing JSON data from the output string
+	if err := json.Unmarshal([]byte(jsonData), &metadata); err != nil {
+		return true
+	}
+
+	return false
 }

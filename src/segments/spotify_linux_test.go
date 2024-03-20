63e6f3280f27bb7e46df88fddc8936a411d9e64c
luisegarduno <luis.e.garduno@gmail.com>
2024-03-20 13:46:39 -0400

    - Changed: RunShellCommand() --> RunCommand()

--- src/segments/spotify_linux_test.go
+++ src/segments/spotify_linux_test.go
@@ -23,7 +23,7 @@ func TestSpotifyLinux(t *testing.T) {
 	}{
 		{
 			Case:            "Playing",
-			ExpectedString:  "\ue602 Snow in Stockholm - Sarah, the Illstrumentalist",
+			ExpectedString:  "Sarah, the Illstrumentalist - Snow in Stockholm",
 			ExpectedEnabled: true,
 			Title:           "Snow in Stockholm - Sarah, the Illstrumentalist",
 			Artist:          "Sarah, the Illstrumentalist",
@@ -31,19 +31,24 @@ func TestSpotifyLinux(t *testing.T) {
 		},
 		{
 			Case:            "Stopped",
-			ExpectedEnabled: false,
-			Title:           "Spotify - Web Player",
+			ExpectedEnabled: true,
+			ExpectedString:  "-",
 		},
 	}
 	for _, tc := range cases {
 		env := new(mock.MockedEnvironment)
 
-		env.On("RunShellCommand", "bash", "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata").Return(tc.Title, tc.Artist)
+		env.On("RunCommand", "dbus-send", []string{
+			"--print-reply",
+			"--dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata",
+		}).Return(tc.Track+" "+tc.Artist, nil)
 
 		s := &Spotify{
-			env: env,
-			props: properties.Map{
-				Command: "echo hi",
+			env:   env,
+			props: properties.Map{},
+			MusicPlayer: MusicPlayer{
+				Artist: tc.Artist,
+				Track:  tc.Track,
 			},
 		}
 		enable := s.Enable()

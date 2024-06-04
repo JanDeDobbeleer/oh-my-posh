f05de03a3f49a7bff8618ab1f5818c86f98a44eb
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-06-04 10:54:55 +0200

    chore(devcontainer): use correct image and settings

--- .devcontainer/Microsoft.PowerShell_profile.ps1
+++ .devcontainer/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,4 @@ Import-Module Terminal-Icons
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 $env:POSH_GIT_ENABLED=$true
-oh-my-posh init pwsh --config $env:POSH_THEME | Invoke-Expression
-
-# NOTE: You can override the above env var from the devcontainer.json "args" under the "build" key.
+oh-my-posh init pwsh | Invoke-Expression

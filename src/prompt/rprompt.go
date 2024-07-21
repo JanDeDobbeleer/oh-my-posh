cdfd249b281a6be099b7c6f7c318312a835eda57
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-07-21 15:00:33 +0200

    fix(bash): remove rprompt support
    BREAKING CHANGE: rprompt is no longer supported for bash due to too many
    side effects that have no solution in sight.

--- src/prompt/rprompt.go
+++ src/prompt/rprompt.go
@@ -1,35 +1,23 @@
 package prompt
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/jandedobbeleer/oh-my-posh/src/config"
-	"github.com/jandedobbeleer/oh-my-posh/src/log"
-	"github.com/jandedobbeleer/oh-my-posh/src/shell"
-	"github.com/jandedobbeleer/oh-my-posh/src/terminal"
 )
 
 func (e *Engine) RPrompt() string {
 	var rprompt *config.Block
-	var lineCount int
 
 	for _, block := range e.Config.Blocks {
-		if block.Type == config.RPrompt {
-			rprompt = block
+		if block.Type != config.RPrompt {
+			continue
 		}
 
-		if block.Newline || block.Type == config.LineBreak {
-			lineCount++
-		}
+		rprompt = block
+		break
 	}
 
 	if rprompt == nil {
 		return ""
-	}
-
-	if e.Env.Shell() == shell.BASH {
-		terminal.Init(shell.GENERIC)
 	}
 
 	rprompt.Init(e.Env)
@@ -40,29 +28,6 @@ func (e *Engine) RPrompt() string {
 
 	text, length := e.renderBlockSegments(rprompt)
 	e.rpromptLength = length
-
-	if e.Env.Shell() != shell.BASH {
-		return text
-	}
-
-	width, err := e.Env.TerminalWidth()
-	if err != nil {
-		log.Error(err)
-		return ""
-	}
-
-	padding := width - e.rpromptLength
-	if padding < 0 {
-		padding = 0
-	}
-
-	text = fmt.Sprintf("%s%s\r", strings.Repeat(" ", padding), text)
-
-	// bash prints this on the same line as the prompt so we need to move the cursor down
-	// in case the prompt spans multiple lines
-	if lineCount > 0 {
-		return terminal.SaveCursorPosition() + fmt.Sprintf("\x1b[%bB", lineCount) + text + terminal.RestoreCursorPosition()
-	}
 
 	return text
 }

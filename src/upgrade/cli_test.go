68caf8a167ca141d47beffd83e27d17f4aae1f96
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-07-22 07:45:18 +0200

    feat(upgrade): do not upgrade to major versions automatically
    resolves #5273

---
+++ src/upgrade/cli_test.go
@@ -0,0 +1,25 @@
+package upgrade
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsMajorUpgrade(t *testing.T) {
+	cases := []struct {
+		Case           string
+		Expected       bool
+		CurrentVersion string
+		LatestVersion  string
+	}{
+		{Case: "Same version", Expected: false, CurrentVersion: "v3.0.0", LatestVersion: "v3.0.0"},
+		{Case: "Breaking change", Expected: true, CurrentVersion: "v3.0.0", LatestVersion: "v4.0.0"},
+		{Case: "Empty version, mostly development build", Expected: false, LatestVersion: "v4.0.0"},
+	}
+
+	for _, tc := range cases {
+		canUpgrade := IsMajorUpgrade(tc.CurrentVersion, tc.LatestVersion)
+		assert.Equal(t, tc.Expected, canUpgrade, tc.Case)
+	}
+}

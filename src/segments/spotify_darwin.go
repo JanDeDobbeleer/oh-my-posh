4647417b5911335de1c22aac17054bedf5d8dedf
Adi Vaknin <6841988+DeepSpace2@users.noreply.github.com>
2024-07-25 17:01:50 +0300

    feat(spotify): add segment cache on macOS

--- src/segments/spotify_darwin.go
+++ src/segments/spotify_darwin.go
@@ -2,7 +2,20 @@
 
 package segments
 
+import (
+	"encoding/json"
+
+	"github.com/jandedobbeleer/oh-my-posh/src/properties"
+)
+
+const spotifyCacheKey = "spotify_music_player"
+
 func (s *Spotify) Enabled() bool {
+	cacheTimeout := s.props.GetInt(properties.CacheTimeout, 0)
+	if cacheTimeout > 0 && s.getFromCache() {
+		return true
+	}
+
 	// Check if running
 	running := s.runAppleScriptCommand("application \"Spotify\" is running")
 	if running == "false" || running == "" {
@@ -23,7 +36,12 @@ func (s *Spotify) Enabled() bool {
 
 	s.Artist = s.runAppleScriptCommand("tell application \"Spotify\" to artist of current track as string")
 	s.Track = s.runAppleScriptCommand("tell application \"Spotify\" to name of current track as string")
+
 	s.resolveIcon()
+
+	if cacheTimeout > 0 {
+		s.setCache(cacheTimeout)
+	}
 
 	return true
 }
@@ -31,4 +49,29 @@ func (s *Spotify) Enabled() bool {
 func (s *Spotify) runAppleScriptCommand(command string) string {
 	val, _ := s.env.RunCommand("osascript", "-e", command)
 	return val
+}
+
+func (s *Spotify) getFromCache() bool {
+	str, found := s.env.Cache().Get(spotifyCacheKey)
+	if !found {
+		return false
+	}
+
+	var cachedMusicPlayer MusicPlayer
+	err := json.Unmarshal([]byte(str), &cachedMusicPlayer)
+	if err != nil {
+		return false
+	}
+
+	s.MusicPlayer = cachedMusicPlayer
+	return true
+}
+
+func (s *Spotify) setCache(cacheTimeout int) {
+	cache, err := json.Marshal(s.MusicPlayer)
+	if err != nil {
+		return
+	}
+
+	s.env.Cache().Set(spotifyCacheKey, string(cache), cacheTimeout)
 }

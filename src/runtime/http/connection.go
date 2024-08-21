a0dffad62d7849e2b0d0f40dbe3b33f7fbf6e05d
Jan De Dobbeleer <2492783+JanDeDobbeleer@users.noreply.github.com>
2024-08-21 07:34:00 +0000

    fix(notice): check connection using ohmyposh.dev
    resolves #5485

--- src/runtime/http/connection.go
+++ src/runtime/http/connection.go
@@ -5,10 +5,10 @@ import (
 	"time"
 )
 
-// if we can connect to google within 200ms, we are connected
+// if we can connect to ohmyposh within 200ms, we are connected
 // otherwise, let's consider being offline
 func IsConnected() bool {
 	timeout := 200 * time.Millisecond
-	_, err := net.DialTimeout("tcp", "google.com:80", timeout)
+	_, err := net.DialTimeout("tcp", "ohmyposh.dev:80", timeout)
 	return err == nil
 }

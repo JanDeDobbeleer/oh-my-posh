cb2dd07b198199566773fe78ae53e40bf5b9508f
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-09-08 11:21:51 +0200

    docs: add upgrade section
    resolves #5452

--- website/sidebars.js
+++ website/sidebars.js
@@ -24,6 +24,7 @@ module.exports = {
             "installation/fonts",
             "installation/prompt",
             "installation/customize",
+            "installation/upgrade",
           ],
         },
       ],

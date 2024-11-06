a88457436e5a695cda30bc147c2ac2e2b4121daf
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-11-06 17:02:19 +0100

    fix(upgrade): restore caching mechanism

--- src/segments/upgrade_test.go
+++ src/segments/upgrade_test.go
@@ -6,11 +6,13 @@ import (
 	"testing"
 
 	"github.com/jandedobbeleer/oh-my-posh/src/build"
+	cache_ "github.com/jandedobbeleer/oh-my-posh/src/cache/mock"
 	"github.com/jandedobbeleer/oh-my-posh/src/properties"
 	"github.com/jandedobbeleer/oh-my-posh/src/runtime/mock"
 	"github.com/jandedobbeleer/oh-my-posh/src/upgrade"
 
 	"github.com/alecthomas/assert"
+	testify_ "github.com/stretchr/testify/mock"
 )
 
 func TestUpgrade(t *testing.T) {
@@ -39,20 +41,41 @@ func TestUpgrade(t *testing.T) {
 			Error: errors.New("error"),
 		},
 		{
+			Case:            "On previous, from cache",
+			HasCache:        true,
+			CurrentVersion:  "1.0.2",
+			LatestVersion:   "1.0.3",
+			CachedVersion:   "1.0.2",
+			ExpectedEnabled: true,
+		},
+		{
 			Case:           "On latest, version changed",
+			HasCache:       true,
 			CurrentVersion: "1.0.2",
 			LatestVersion:  "1.0.2",
+			CachedVersion:  "1.0.1",
 		},
 		{
 			Case:            "On previous, version changed",
+			HasCache:        true,
 			CurrentVersion:  "1.0.2",
 			LatestVersion:   "1.0.3",
+			CachedVersion:   "1.0.1",
 			ExpectedEnabled: true,
 		},
 	}
 
 	for _, tc := range cases {
 		env := new(mock.Environment)
+		cache := &cache_.Cache{}
+
+		env.On("Cache").Return(cache)
+		if len(tc.CachedVersion) == 0 {
+			tc.CachedVersion = tc.CurrentVersion
+		}
+		cacheData := fmt.Sprintf(`{"latest":"%s", "current": "%s"}`, tc.LatestVersion, tc.CachedVersion)
+		cache.On("Get", UPGRADECACHEKEY).Return(cacheData, tc.HasCache)
+		cache.On("Set", testify_.Anything, testify_.Anything, testify_.Anything)
 
 		build.Version = tc.CurrentVersion
 

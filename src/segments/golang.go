9d285d74a4d64b56c62b3fd02915ae8c6e276987
Bosco Domingo <boscodomingob@gmail.com>
2025-01-03 17:22:18 +0100

    feat: add go.sum and go.work to Go detection

--- src/segments/golang.go
+++ src/segments/golang.go
@@ -2,7 +2,7 @@ package segments
 
 import (
 	"github.com/jandedobbeleer/oh-my-posh/src/properties"
-
+	"github.com/jandedobbeleer/oh-my-posh/src/regex"
 	"golang.org/x/mod/modfile"
 )
 
@@ -11,7 +11,8 @@ type Golang struct {
 }
 
 const (
-	ParseModFile properties.Property = "parse_mod_file"
+	ParseModFile  properties.Property = "parse_mod_file"
+	ParseWorkFile properties.Property = "parse_work_file"
 )
 
 func (g *Golang) Template() string {
@@ -19,7 +20,7 @@ func (g *Golang) Template() string {
 }
 
 func (g *Golang) Enabled() bool {
-	g.extensions = []string{"*.go", "go.mod"}
+	g.extensions = []string{"*.go", "go.mod", "go.sum", "go.work", "go.work.sum"}
 	g.commands = []*cmd{
 		{
 			regex:      `(?P<version>((?P<major>[0-9]+).(?P<minor>[0-9]+)(.(?P<patch>[0-9]+))?))`,
@@ -36,14 +37,26 @@ func (g *Golang) Enabled() bool {
 	return g.language.Enabled()
 }
 
+// getVersion returns the version of the Go language
+// It first checks if the go.mod file is present and if it is, it parses the file to get the version
+// If the go.mod file is not present, it checks if the go.work file is present and if it is, it parses the file to get the version
+// If neither file is present, it returns an empty string
 func (g *Golang) getVersion() (string, error) {
-	if !g.props.GetBool(ParseModFile, false) {
-		return "", nil
+	if g.props.GetBool(ParseModFile, false) {
+		return g.parseModFile()
 	}
 
+	if g.props.GetBool(ParseWorkFile, false) {
+		return g.parseWorkFile()
+	}
+
+	return "", nil
+}
+
+func (g *Golang) parseModFile() (string, error) {
 	gomod, err := g.language.env.HasParentFilePath("go.mod", false)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 
 	contents := g.language.env.FileContent(gomod.Path)
@@ -52,5 +65,26 @@ func (g *Golang) getVersion() (string, error) {
 		return "", err
 	}
 
-	return file.Go.Version, nil
+	if file.Go.Version != "" {
+		return file.Go.Version, nil
+	}
+
+	// ignore when no version is found in go.mod file
+	return "", nil
+}
+
+func (g *Golang) parseWorkFile() (string, error) {
+	goWork, err := g.language.env.HasParentFilePath("go.work", false)
+	if err != nil {
+		return "", err
+	}
+
+	contents := g.language.env.FileContent(goWork.Path)
+	version := regex.FindStringMatch(`go (\d(\.\d{1,2})?(\.\d{1,2})?)`, contents, 1)
+	if len(version) > 0 {
+		return version, nil
+	}
+
+	// ignore when no version is found in go.work file
+	return "", nil
 }

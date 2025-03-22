644201abbcecdaa4fb85f9fa35eca2060bd0195c
luisegarduno <luis.e.garduno@gmail.com>
2025-03-22 02:59:11 -0400

    feat(spotify): add linux support

--- src/segments/spotify_linux.go
+++ src/segments/spotify_linux.go
@@ -9,32 +9,75 @@ import (
 )
 
 func (s *Spotify) Enabled() bool {
-	var err error
-	running := s.runLinuxScriptCommand("")
+	// Check if we're in WSL and handle that separately
+	if s.env.IsWsl() {
+		return s.enabledWsl()
+	}
 
-	if strings.HasPrefix(running, "ERROR") {
+	// Standard Linux implementation
+	running := s.runLinuxScriptCommand(" string:PlaybackStatus | awk -F '\"' '/string/ {print tolower($2)}'")
+
+	if strings.HasPrefix(running, "Error") {
 		return false
 	}
 	if strings.Contains(running, "Error.ServiceUnknown") || strings.HasSuffix(running, "-") {
 		s.Status = stopped
 		return false
 	}
-	s.Status = playing
-	if err != nil {
+
+	if running == stopped {
 		s.Status = stopped
 		return false
 	}
-	if s.Status == stopped {
-		return false
-	}
 
-	s.Artist = s.runLinuxScriptCommand(" | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:artist'/ {a=$4} END {print a}'")
-	s.Track = s.runLinuxScriptCommand(" | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:title'/ {t=$4} END {print t}'")
+	s.Status = running
+	s.Artist = s.runLinuxScriptCommand(" string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:artist'/ {a=$4} END {print a}'")
+	s.Track = s.runLinuxScriptCommand(" string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:title'/ {t=$4} END {print t}'")
 	s.resolveIcon()
 	return true
 }
 
 func (s *Spotify) runLinuxScriptCommand(command string) string {
-	val := s.env.RunShellCommand(shell.BASH, "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata"+command)
+	dbusCMD := "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player"
+	val := s.env.RunShellCommand(shell.BASH, dbusCMD+command)
 	return val
+}
+
+// Implementation for WSL - moved from spotify_wsl.go
+func (s *Spotify) enabledWsl() bool {
+	tlist, err := s.env.RunCommand("tasklist.exe", "/V", "/FI", "Imagename eq Spotify.exe", "/FO", "CSV", "/NH")
+	if err != nil || strings.HasPrefix(tlist, "INFO") {
+		return false
+	}
+
+	records := strings.Split(tlist, "\n")
+	if len(records) == 0 {
+		return false
+	}
+
+	for _, record := range records {
+		record = strings.TrimSpace(record)
+		fields := strings.Split(record, ",")
+		if len(fields) == 0 {
+			continue
+		}
+
+		// last elemant is the title
+		title := fields[len(fields)-1]
+		// trim leading and trailing quotes from the field
+		title = strings.TrimPrefix(title, `"`)
+		title = strings.TrimSuffix(title, `"`)
+		if !strings.Contains(title, " - ") {
+			continue
+		}
+
+		infos := strings.Split(title, " - ")
+		s.Artist = infos[0]
+		s.Track = strings.Join(infos[1:], " - ")
+		s.Status = playing
+		s.resolveIcon()
+		return true
+	}
+
+	return false
 }
--- src/segments/spotify_linux_test.go
+++ src/segments/spotify_linux_test.go
@@ -6,8 +6,8 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/jandedobbeleer/oh-my-posh/src/mock"
 	"github.com/jandedobbeleer/oh-my-posh/src/properties"
+	"github.com/jandedobbeleer/oh-my-posh/src/runtime/mock"
 	"github.com/jandedobbeleer/oh-my-posh/src/shell"
 
 	"github.com/stretchr/testify/assert"
@@ -22,21 +22,23 @@ func TestSpotifyLinux(t *testing.T) {
 		Track    string
 		Error    error
 	}{
-		{Running: false, Expected: "\uE602  -", Error: errors.New("oops")},
+		{Running: false, Expected: ""},
+		{Running: false, Expected: "", Error: errors.New("oops")},
+		{Running: true, Expected: "\uF8E3 Candlemass - Spellbreaker", Status: "paused", Artist: "Candlemass", Track: "Spellbreaker"},
 		{Running: true, Expected: "\uE602 Candlemass - Spellbreaker", Status: "playing", Artist: "Candlemass", Track: "Spellbreaker"},
-		{Running: true, Expected: "\uE602 Candlemass - Spellbreaker", Status: "paused", Artist: "Candlemass", Track: "Spellbreaker"},
 	}
 	for _, tc := range cases {
-		env := new(mock.MockedEnvironment)
+		env := new(mock.Environment)
 		env.On("IsWsl").Return(false)
-		env.On("RunShellCommand", shell.BASH, "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata").Return(tc.Status, nil)
-		env.On("RunShellCommand", shell.BASH, "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:artist'/ {a=$4} END {print a}'").Return(tc.Artist, nil)
-		env.On("RunShellCommand", shell.BASH, "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:title'/ {t=$4} END {print t}'").Return(tc.Track, nil)
 
-		s := &Spotify{
-			env:   env,
-			props: properties.Map{},
-		}
+		dbusCMD := "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player"
+		env.On("RunShellCommand", shell.BASH, dbusCMD+" string:PlaybackStatus | awk -F '\"' '/string/ {print tolower($2)}'").Return(tc.Status, nil)
+		env.On("RunShellCommand", shell.BASH, dbusCMD+" string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:artist'/ {a=$4} END {print a}'").Return(tc.Artist, nil)
+		env.On("RunShellCommand", shell.BASH, dbusCMD+" string:Metadata | awk -F '\"' 'BEGIN {RS=\"entry\"}; /'xesam:title'/ {t=$4} END {print t}'").Return(tc.Track, nil)
+
+		s := &Spotify{}
+		s.Init(properties.Map{}, env)
+
 		enable := s.Enabled()
 		assert.True(t, enable)
 		if tc.Running {

8b46fc06f6c16a46fe8a3b6d3d7b93b00183a094
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-04-17 07:58:49 +0200

    fix(engine): safeguard previous segment's existance

--- src/prompt/engine.go
+++ src/prompt/engine.go
@@ -392,7 +392,7 @@ func (e *Engine) writeSeparator(final bool) {
 		bgColor = color.Background
 	}
 
-	if e.activeSegment.InvertPowerline || e.previousActiveSegment.InvertPowerline {
+	if e.activeSegment.InvertPowerline || (e.previousActiveSegment != nil && e.previousActiveSegment.InvertPowerline) {
 		terminal.Write(e.getPowerlineColor(), bgColor, symbol)
 		return
 	}

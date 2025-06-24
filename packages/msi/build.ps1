3bf3592cc38eb35e6fdd54d262e42524f4cf24ea
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-06-24 20:54:11 +0200

    feat(windows): add msix installer
    resolves #6553

--- packages/msi/build.ps1
+++ packages/msi/build.ps1
@@ -13,7 +13,14 @@ Param
     [switch]$Copy
 )
 
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
 $PSDefaultParameterValues['Out-File:Encoding'] = 'UTF8'
+
+Write-Host "Building MSI for $Architecture with version $Version"
+
+Write-Host "Setting up folders"
 
 New-Item -Path "." -Name "dist" -ItemType Directory -ErrorAction SilentlyContinue
 New-Item -Path "." -Name "out" -ItemType Directory -ErrorAction SilentlyContinue
@@ -25,6 +32,8 @@ if ($Copy) {
         Default { $file = "posh-windows-$Architecture.exe" }
     }
 
+    Write-Host "Copying $file to ./dist/oh-my-posh.exe"
+
     # copy the correct architecture to ./dist
     Copy-Item -Path "../../dist/$file" -Destination "./dist/oh-my-posh.exe"
 }
@@ -32,12 +41,16 @@ if ($Copy) {
 # variables
 $env:VERSION = $Version
 
+Write-Host "Creating MSI package"
+
 # create MSI
 $fileName = "install-$Architecture.msi"
-$installer = "./out/$fileName"
+$installer = "$PWD/out/$fileName" -replace '\\', '/'
 wix build -arch $Architecture -out $installer .\oh-my-posh.wxs
 
 if ($Sign) {
+    Write-Host "Signing MSI"
+
     # setup dependencies
     nuget.exe install Microsoft.Trusted.Signing.Client -Version 1.0.92 -x
     $signtoolDlib = "$PWD/Microsoft.Trusted.Signing.Client/bin/x64/Azure.CodeSigning.Dlib.dll"
@@ -48,9 +61,67 @@ if ($Sign) {
     $signtoolDlib = $signtoolDlib -Replace '\\', '/'
 
     # sign installer
-    & "$signtool" sign /v /debug /fd SHA256 /tr 'http://timestamp.acs.microsoft.com' /td SHA256 /dlib "$signtoolDlib" /dmdf ../../src/metadata.json $installer
+    & $signtool sign /v /debug /fd SHA256 /tr 'http://timestamp.acs.microsoft.com' /td SHA256 /dlib "$signtoolDlib" /dmdf ../../src/metadata.json "$installer"
 }
 
-# get hash
-$zipHash = Get-FileHash $installer -Algorithm SHA256
-$zipHash.Hash | Out-File -Encoding 'UTF8' "$installer.sha256"
+Write-Host "Creating MSIX package"
+
+# msix
+$current = $PWD -replace '\\', '/'
+$manifest = "$current/appxmanifest.xml"
+$mappingTxt = "$current/mapping.txt"
+$installerMSIX = "$current/out/$($filename)x"
+
+[xml]$XmlDocument = Get-Content $manifest
+
+$XmlDocument.Package.Identity.Version = "$Version.0"
+$XmlDocument.Package.Identity.ProcessorArchitecture = $Architecture
+
+$XmlDocument.Save($manifest)
+
+Write-Host "Creating mapping file"
+
+$mapping = @"
+[ResourceMetadata]
+"ResourceDimensions"                    "language-en-us"
+"ResourceId"                            "English"
+
+[Files]
+"./dist/oh-my-posh.exe" "oh-my-posh.exe"
+"./icons/icon.png" "/icons/icon.png"
+"./icons/44.png" "/icons/44.png"
+"@.Trim()
+
+$stringBuilder = New-Object -TypeName System.Text.StringBuilder
+$stringBuilder.Append($mapping) | Out-Null
+
+# Add all theme files to the mapping and add to $mapping
+Get-ChildItem -Path "../../themes" -Recurse -File -Filter "*.omp.*" | ForEach-Object {
+    $stringBuilder.Append("`n`"../../themes/$($_.Name)`" `"/themes/$($_.Name)`"") | Out-Null
+}
+
+$stringBuilder.ToString() | Out-File -FilePath $mappingTxt
+
+$makeappx = "C:/Program Files (x86)/Windows Kits/10/bin/$SDKVersion/x64/makeappx.exe"
+
+& "$makeappx" pack /p $installerMSIX /v /o /m $manifest /f $mappingTxt
+
+if ($Sign) {
+    Write-Host "Signing MSIX"
+    & "$signtool" sign /v /debug /fd SHA256 /tr 'http://timestamp.acs.microsoft.com' /td SHA256 /dlib "$signtoolDlib" /dmdf ../../src/metadata.json "$installerMSIX"
+}
+
+Write-Host "Creating hash files"
+
+function Set-FileHash {
+    param (
+        [string]$File
+    )
+    $hash = Get-FileHash -Path $File -Algorithm SHA256
+    $hash.Hash | Out-File -Encoding 'UTF8' "$File.sha256"
+}
+
+Set-FileHash -File $installer
+Set-FileHash -File $installerMSIX
+
+Write-Host "Finished building MSI and MSIX packages"

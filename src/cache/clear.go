459c0d67ea20be70fee5c5e33fe142767c64dec3
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-06-29 10:37:06 +0200

    feat(clear): remove all log files

--- src/cache/clear.go
+++ src/cache/clear.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/jandedobbeleer/oh-my-posh/src/log"
 )
 
 func Clear(cachePath string, force bool) ([]string, error) {
@@ -53,5 +55,59 @@ func Clear(cachePath string, force bool) ([]string, error) {
 		deleteFile(file.Name())
 	}
 
+	deletedLogs := deleteLogs(force)
+	if len(deletedLogs) > 0 {
+		removed = append(removed, deletedLogs...)
+	}
+
 	return removed, nil
+}
+
+func deleteLogs(force bool) []string {
+	var removed []string
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Error(err)
+		return removed
+	}
+
+	logPath := filepath.Join(home, ".oh-my-posh")
+
+	deleteFile := func(file string) {
+		path := filepath.Join(logPath, file)
+		if err := os.Remove(path); err == nil {
+			removed = append(removed, path)
+		}
+	}
+
+	logFiles, err := os.ReadDir(logPath)
+	if err != nil {
+		log.Error(err)
+		return removed
+	}
+
+	for _, file := range logFiles {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".log") {
+			continue
+		}
+
+		if force {
+			deleteFile(file.Name())
+			continue
+		}
+
+		info, err := file.Info()
+		if err != nil {
+			continue
+		}
+
+		if info.ModTime().AddDate(0, 0, 7).After(info.ModTime()) {
+			continue
+		}
+
+		deleteFile(file.Name())
+	}
+
+	return removed
 }

7c36ff4ef6553d9441200f3d8445bceff867fa35
Lucas Costi <lucascosti@canva.com>
2025-07-10 11:12:12 +1000

    chore: update Node LTS version numbers

--- src/segments/node.go
+++ src/segments/node.go
@@ -135,9 +135,9 @@ func (n *Node) matchesVersionFile() (string, bool) {
 		case "hydrogen":
 			fileVersion = "18.20.8"
 		case "iron":
-			fileVersion = "20.19.2"
+			fileVersion = "20.19.3"
 		case "jod":
-			fileVersion = "22.16.0"
+			fileVersion = "22.17.0"
 		}
 	}
 

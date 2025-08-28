00b547da71366d6d6689d9572971f51a68c756d1
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-08-28 10:08:57 +0200

    fix(msix): return actual path for cache

---
+++ src/cache/path_unix.go
@@ -0,0 +1,13 @@
+//go:build !windows
+
+package cache
+
+import "os"
+
+func platformCachePath() (string, bool) {
+	if cachePath, OK := returnOrBuildCachePath(os.Getenv("XDG_CACHE_HOME")); OK {
+		return cachePath, true
+	}
+
+	return "", false
+}

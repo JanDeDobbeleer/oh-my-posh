28aef9b469dd43d3b8faa6bba2c043cc5eb04cb9
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-10-02 09:57:55 +0200

    fix(time): map time constants
    resolves #6836

--- src/segments/time.go
+++ src/segments/time.go
@@ -24,9 +24,43 @@ func (t *Time) Template() string {
 
 func (t *Time) Enabled() bool {
 	// if no date set, use now(unit testing)
-	t.Format = t.props.GetString(TimeFormat, "15:04:05")
+	formatInput := t.props.GetString(TimeFormat, "15:04:05")
+	t.Format = t.getTimeFormat(formatInput)
 	if t.CurrentDate.IsZero() {
 		t.CurrentDate = time.Now()
 	}
+
 	return true
+}
+
+var timeFormatLookup = map[string]string{
+	// Maps string names to their corresponding time package constants
+	"Layout":      time.Layout,      // "01/02 03:04:05PM '06 -0700"
+	"ANSIC":       time.ANSIC,       // "Mon Jan _2 15:04:05 2006"
+	"UnixDate":    time.UnixDate,    // "Mon Jan _2 15:04:05 MST 2006"
+	"RubyDate":    time.RubyDate,    // "Mon Jan 02 15:04:05 -0700 2006"
+	"RFC822":      time.RFC822,      // "02 Jan 06 15:04 MST"
+	"RFC822Z":     time.RFC822Z,     // "02 Jan 06 15:04 -0700"
+	"RFC850":      time.RFC850,      // "Monday, 02-Jan-06 15:04:05 MST"
+	"RFC1123":     time.RFC1123,     // "Mon, 02 Jan 2006 15:04:05 MST"
+	"RFC1123Z":    time.RFC1123Z,    // "Mon, 02 Jan 2006 15:04:05 -0700"
+	"RFC3339":     time.RFC3339,     // "2006-01-02T15:04:05Z07:00"
+	"RFC3339Nano": time.RFC3339Nano, // "2006-01-02T15:04:05.999999999Z07:00"
+	"Kitchen":     time.Kitchen,     // "3:04PM"
+	"Stamp":       time.Stamp,       // "Jan _2 15:04:05"
+	"StampMilli":  time.StampMilli,  // "Jan _2 15:04:05.000"
+	"StampMicro":  time.StampMicro,  // "Jan _2 15:04:05.000000"
+	"StampNano":   time.StampNano,   // "Jan _2 15:04:05.000000000"
+	"DateTime":    time.DateTime,    // "2006-01-02 15:04:05"
+	"DateOnly":    time.DateOnly,    // "2006-01-02"
+	"TimeOnly":    time.TimeOnly,    // "15:04:05"
+}
+
+// getTimeFormat returns the time format constant if the input matches a known format name,
+// otherwise returns the input unchanged
+func (t *Time) getTimeFormat(format string) string {
+	if timeFormat, exists := timeFormatLookup[format]; exists {
+		return timeFormat
+	}
+	return format
 }

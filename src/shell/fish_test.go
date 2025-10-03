992882ce2a122c72b47798836ea84e98d06a102f
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2025-10-03 14:36:10 +0200

    feat(fish)!: use built-in transient prompt
    BREAKING CHANGE: this requires fish 4.1.0

--- src/shell/fish_test.go
+++ src/shell/fish_test.go
@@ -12,7 +12,7 @@ func TestFishFeatures(t *testing.T) {
 
 	want := `// these are the features
 enable_poshtooltips
-set --global _omp_transient_prompt 1
+set --global fish_transient_prompt 1
 set --global _omp_ftcs_marks 1
 "$_omp_executable" upgrade
 "$_omp_executable" notice

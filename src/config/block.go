103a8c6c36dbff06ce4fb3f56de8cea64c619832
JamBalaya56562 <jambalaya.pyoncafe@outlook.jp>
2026-01-02 19:25:20 +0900

    feat: remove unused block properties

--- src/config/block.go
+++ src/config/block.go
@@ -35,8 +35,6 @@ type Block struct {
 	LeadingDiamond  string         `json:"leading_diamond,omitempty" toml:"leading_diamond,omitempty" yaml:"leading_diamond,omitempty"`
 	TrailingDiamond string         `json:"trailing_diamond,omitempty" toml:"trailing_diamond,omitempty" yaml:"trailing_diamond,omitempty"`
 	Segments        []*Segment     `json:"segments,omitempty" toml:"segments,omitempty" yaml:"segments,omitempty"`
-	MaxWidth        int            `json:"max_width,omitempty" toml:"max_width,omitempty" yaml:"max_width,omitempty"`
-	MinWidth        int            `json:"min_width,omitempty" toml:"min_width,omitempty" yaml:"min_width,omitempty"`
 	Newline         bool           `json:"newline,omitempty" toml:"newline,omitempty" yaml:"newline,omitempty"`
 	Force           bool           `json:"force,omitempty" toml:"force,omitempty" yaml:"force,omitempty"`
 	Index           int            `json:"index,omitempty" toml:"index,omitempty" yaml:"index,omitempty"`

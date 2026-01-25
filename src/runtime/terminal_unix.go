47ae861da3e6a161424e793e1eeab984bbfe4fb8
MrRainbow0704 <2007.marcosimone@gmail.com>
2026-01-25 22:14:47 +0100

    fix: fixed memorization of wrong value inside the enviroment_platform cache variable

--- src/runtime/terminal_unix.go
+++ src/runtime/terminal_unix.go
@@ -103,7 +103,7 @@ func (term *Terminal) Platform() string {
 	}()
 
 	if wsl := term.Getenv("WSL_DISTRO_NAME"); len(wsl) != 0 {
-		platform, _, _ := strings.Cut(wsl, "-")
+		platform, _, _ = strings.Cut(wsl, "-")
 		platform = strings.ToLower(platform)
 		log.Debug(platform)
 		return platform
